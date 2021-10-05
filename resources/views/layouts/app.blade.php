b19713d0df3b618d676cda5d14e625dfbcc3192d
hamis-juma <hamisjuma1@gmail.com>
2021-10-06 02:30:50 +0300

    Theme setup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,132 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Meta data -->
+    <meta charset="UTF-8">
+    <meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
+    <meta content="Clont - Bootstrap Webapp Responsive Dashboard Simple Admin Panel Premium HTML5 Template" name="description">
+    <meta content="Spruko Technologies Private Limited" name="author">
+    <meta name="keywords" content="Admin, Admin Template, Dashboard, Responsive, Admin Dashboard, Bootstrap, Bootstrap 4, Clean, Backend, Jquery, Modern, Web App, Admin Panel, Ui, Premium Admin Templates, Flat, Admin Theme, Ui Kit, Bootstrap Admin, Responsive Admin, Application, Template, Admin Themes, Dashboard Template"/>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Title -->
+    <title>Clont - Bootstrap Webapp Responsive Dashboard Simple Admin Panel Premium HTML5 Template</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!--Favicon -->
+    <link rel="icon" href="mdh/images/brand/favicon.ico" type="image/x-icon"/>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <!-- Style css -->
+{{ Html::style(url('mdh/css/style.css')) }}
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<!--Horizontal css -->
+    <link id="effect" href="mdh/plugins/horizontal-menu/dropdown-effects/fade-up.css" rel="stylesheet" />
+
+{{ Html::style(url('mdh/plugins/horizontal-menu/horizontal.css')) }}
+
+<!--Sidemenu css -->
+{{ Html::style(url('mdh/plugins/sidemenu/combine-menu/combine-menu.css')) }}
+
+<!-- P-scroll bar css-->
+{{ Html::style(url('mdh/plugins/p-scrollbar/p-scrollbar.css')) }}
+
+<!---Icons css-->
+{{ Html::style(url('mdh/plugins/web-fonts/icons.css')) }}
+{{ Html::style(url('mdh/plugins/web-fonts/font-awesome/font-awesome.min.css')) }}
+{{ Html::style(url('mdh/plugins/web-fonts/plugin.css')) }}
+
+<!-- Skin css-->
+    {{ Html::style(url('mdh/css/skins.css')) }}
+
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+<body class="app sidebar-mini">
 
-                    </ul>
+<!---Global-loader-->
+<div id="global-loader" >
+    <img src="mdh/images/svgs/loader.svg" alt="loader">
+</div>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+<div class="page comb-page">
+    <div class="page-main">
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+        {{--Header--}}
+            @include('includes.navigation.header')
+        {{--Header closed--}}
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+        <!--aside open-->
+        @include('includes.navigation.aside')
+        <!--aside closed-->
+
+        <div class="app-content page-body">
+
+            <!-- Horizontal-menu -->
+{{--            @include('includes.navigation.horizontal')--}}
+            <!-- Horizontal-menu end -->
+
+            {{--main body--}}
+
+            <div class="side-app">
+
+                <!--Page header-->
+{{--                @include('includes.page_header')--}}
+                <!--End Page header-->
+
+                @yield('content')
+
+            </div>
+
+
+            {{--main body closed--}}
+
+        </div><!-- end app-content-->
+    </div>
+
+    <!--Footer-->
+    <footer class="footer">
+        <div class="container">
+            <div class="row align-items-center flex-row-reverse">
+                <div class="col-md-12 col-sm-12 mt-3 mt-lg-0 text-center">
+                    Copyright © 2019 <a href="#">Clont</a>. Designed by <a href="#">Spruko Technologies Pvt.Ltd</a> All rights reserved.
                 </div>
             </div>
-        </nav>
+        </div>
+    </footer>
+    <!-- End Footer-->
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+</div>
+
+<!-- Back to top -->
+<a href="#top" id="back-to-top" style="display: inline;"><i class="fa fa-angle-up"></i></a>
+
+<!-- Jquery js-->
+{!! Html::script(url('mdh/js/vendors/jquery-3.4.0.min.js')) !!}
+
+<!-- Bootstrap4 js-->
+{!! Html::script(url('mdh/plugins/bootstrap/popper.min.js')) !!}
+{!! Html::script(url('mdh/plugins/bootstrap/js/bootstrap.min.js')) !!}
+
+<!--Othercharts js-->
+{!! Html::script(url('mdh/plugins/othercharts/jquery.sparkline.min.js')) !!}
+
+<!-- Circle-progress js-->
+{!! Html::script(url('mdh/js/vendors/circle-progress.min.js')) !!}
+
+<!-- Jquery-rating js-->
+{!! Html::script(url('mdh/plugins/rating/jquery.rating-stars.js')) !!}
+
+<!--Horizontal js-->
+{!! Html::script(url('mdh/plugins/horizontal-menu/horizontal.js')) !!}
+
+<!--Sidemenu js-->
+{!! Html::script(url('mdh/plugins/sidemenu/combine-menu/combine-menu.js')) !!}
+
+<!-- P-scroll js-->
+{!! Html::script(url('mdh/plugins/p-scrollbar/p-scrollbar.js')) !!}
+{!! Html::script(url('mdh/plugins/p-scrollbar/p-scroll1.js')) !!}
+{!! Html::script(url('mdh/plugins/p-scrollbar/p-scroll1.js')) !!}
+
+<!-- Custom js-->
+{!! Html::script(url('mdh/js/custom.js')) !!}
+
 </body>
 </html>
