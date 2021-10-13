e55b4fcdcf66c5e63cccd9c99f3d448aaccf4d84
Frank Omary <fomary@mdh.or.tz>
2021-10-13 13:25:28 +0300

    installed sweet alert and changed icons

--- resources/views/includes/navigation/aside.blade.php
+++ resources/views/includes/navigation/aside.blade.php
@@ -6,9 +6,9 @@
             <a class="side-menu__item" href="{{ route('workspace.invoke') }}"><i class="side-menu__icon fe fe-monitor"></i><span class="side-menu__label">Workspace</span></a>
         </li>
 
-        <li><a class="side-menu__item" href="{{ route('workspace.invoke') }}"><i class="side-menu__icon fe fe-inbox"></i><span class="side-menu__label">New Requests</span></a></li>
-        <li><a class="side-menu__item" href="{{ route('workspace.invoke') }}"><i class="side-menu__icon fe fe-monitor"></i><span class="side-menu__label">Responded Requests</span></a></li>
-        <li><a class="side-menu__item" href="{{ route('workspace.invoke') }}"><i class="side-menu__icon fe fe-monitor"></i><span class="side-menu__label">Actioned Request</span></a></li>
+        <li><a class="side-menu__item" href="{{ route('workspace.invoke') }}"><i class="side-menu__icon ion-archive"></i><span class="side-menu__label">New Requests</span></a></li>
+        <li><a class="side-menu__item" href="{{ route('workspace.invoke') }}"><i class="side-menu__icon ion-reply"></i><span class="side-menu__label">Responded Requests</span></a></li>
+        <li><a class="side-menu__item" href="{{ route('workspace.invoke') }}"><i class="side-menu__icon ion-reply-all"></i><span class="side-menu__label">Actioned Request</span></a></li>
 
         <li class="slide">
             <a class="side-menu__item"  data-toggle="slide" href="#"><i class="side-menu__icon fe fe-users"></i><span class="side-menu__label">User Management</span><i class="angle fa fa-angle-right"></i></a>
@@ -21,7 +21,7 @@
             </ul>
         </li>
         <li class="slide">
-            <a class="side-menu__item"  data-toggle="slide" href="#"><i class="side-menu__icon fe fe-settings"></i><span class="side-menu__label">Settings</span><i class="angle fa fa-angle-right"></i></a>
+            <a class="side-menu__item"  data-toggle="slide" href="#"><i class="side-menu__icon ion-settings"></i><span class="side-menu__label">Settings</span><i class="angle fa fa-angle-right"></i></a>
             <ul class="slide-menu">
                 <li><a class="slide-item"  href="{{ route('project.index') }}"><span>Project</span></a></li>
                 <li><a class="slide-item"  href="#"><span>Budget</span></a></li>
