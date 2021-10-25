c6de7df824ab4ee60a112e596543bc6aa32f2c5c
Frank Omary <fomary@mdh.or.tz>
2021-10-25 14:44:12 +0300

    update show

--- resources/views/project/show.blade.php
+++ resources/views/project/show.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.app')
 @section('content')
-    <div class="row  mb-3">
-        <span class="col-12 font-weight-bold">PROJECTS NAME: {{ $project->title }}</span>
-    </div>
+    {{-- <div class="row  mb-3">
+        <span class="col-12 text-center font-weight-bold">PROJECTS NAME: {{ $project->title }}</span>
+    </div> --}}
     <div class="row">
+        
         <div class="card-body">
             <div class="table-responsive">
                 @include('project.form.edit')
