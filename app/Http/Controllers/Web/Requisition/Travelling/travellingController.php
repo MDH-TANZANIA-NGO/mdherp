19632c80ac0476d4bdfeca00007889cbf8204134
Elinipendo Mziray <emziray@mdh.or.tz>
2021-11-21 22:21:52 +0300

    travelling form submited

--- app/Http/Controllers/Web/Requisition/Travelling/travellingController.php
+++ app/Http/Controllers/Web/Requisition/Travelling/travellingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Web\Requisition\Travelling;
 use App\Http\Controllers\Controller;
 use App\Models\Auth\User;
 use App\Models\MdhRates\mdh_rate;
+use App\Models\Requisition\Travelling\travelling_cost;
 use App\Models\System\District;
 use Illuminate\Http\Request;
 
@@ -35,8 +36,32 @@ class travellingController extends Controller
 
     }
 
-    public function store()
+    public function store( Request $request)
     {
+        $travelling = new travelling_cost();
+        $other_cost = request('other_cost');
+        $no_days = request('no_days');
+        $perdiem_rate = request('perdiem_rate');
+        $district_id = request('district_id');
+        $transportation = request('transportation');
+        $accomodation = request('accomodation');
+        $total_perdiem_rate = $perdiem_rate * $no_days;
+        $total_accomodation = $accomodation * $no_days;
+        $total_amount =  $other_cost + $transportation + $total_perdiem_rate + $total_accomodation;
+
+
+        $travelling-> user_id = request('user_id');
+        $travelling->district_id= $district_id;
+        $travelling-> perdiem_rate = $perdiem_rate;
+        $travelling-> accomodation = $accomodation;
+        $travelling-> no_days = $no_days;
+        $travelling-> transportation = $transportation;
+        $travelling-> other_cost = $other_cost;
+        $travelling->total_amount = $total_amount;
+
+        $travelling->save();
+
+        return redirect()->back();
 
     }
 
--- resources/views/requisition/travelling/forms/create.blade.php
+++ resources/views/requisition/travelling/forms/create.blade.php
@@ -1,10 +1,10 @@
-{!! Form::open(['route' => 'training.store', 'method' => 'post',]) !!}
+{!! Form::open(['route' => 'travelling.store', 'method' => 'post',]) !!}
 <!-- Row -->
 <div class="row">
     <div class="col-md-12 col-lg-12">
         <div class="card">
             <div class="card-header">
-                <h3 class="card-title">TRAINING FORM</h3>
+                <h3 class="card-title">TRAVELLING  FORM</h3>
                 <div class="card-options ">
                     <a href="#" class="card-options-collapse" data-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a>
                     <a href="#" class="card-options-remove" data-toggle="card-remove"><i class="fe fe-x"></i></a>
@@ -13,7 +13,7 @@
             <div class="card-body">
                 <div class="table-responsive">
                     <table class="table card-table table-vcenter text-nowrap" >
-                        <thead  class="bg-primary text-white">
+                        <thead  class="bg-info text-white">
                         <tr >
                             <th class="text-white">Travellor</th>
                             <th class="text-white">Days</th>
@@ -27,29 +27,29 @@
                         </thead>
                         <tbody>
                         <tr>
-                            <td>	 <select class="form-control select2-show-search" data-placeholder="Choose one (with searchbox)">
+                            <td>	 <select class="form-control select2-show-search" name="user_id" data-placeholder="Choose one (with searchbox)">
                                     @foreach($user_id as $user_id)
                                         <option value="{{$user_id->id}}">{{$user_id->first_name}} {{$user_id->last_name}}</option>
 
                                     @endforeach
                                 </select></td>
-                            <td><input type="number" class="form-control" placeholder="No days"></td>
-                            <td>	 <select class="form-control select2-show-search" data-placeholder="Choose one (with searchbox)">
+                            <td><input type="number" class="form-control" name="no_days" placeholder="No days"></td>
+                            <td>	 <select class="form-control select2-show-search" name="district_id" data-placeholder="Choose one (with searchbox)">
                                     @foreach($districts as $districts)
                                         <option value="{{$districts->id}}">{{$districts->name}}</option>
 
                                     @endforeach
                                 </select></td>
-                            <td>	    <select class="form-control select2-show-search" data-placeholder="Choose one (with searchbox)">
+                            <td>	    <select class="form-control select2-show-search" name="perdiem_rate" data-placeholder="Choose one (with searchbox)">
                                     @foreach($mdh_rates as $mdh_rates)
                                         <option value="{{$mdh_rates->amount}}">{{$mdh_rates->amount}}</option>
 
                                     @endforeach
                                 </select></td>
-                            <td> <input type="number" class="form-control" placeholder="100,000"></td>
-                            <td><input type="number" class="form-control" placeholder="100,000"></td>
-                            <td><input type="number" class="form-control" placeholder="100,000"></td>
-                            <td><button type="submit" class="btn-blue">Add</button> </td>
+                            <td> <input type="number" class="form-control" name="accomodation" placeholder="one night cost"></td>
+                            <td><input type="number" class="form-control" name="transportation" placeholder="100,000"></td>
+                            <td><input type="number" class="form-control" name="other_cost" placeholder="100,000"></td>
+                            <td><button type="submit" class="btn btn-outline-info">Add</button> </td>
                         </tr>
 
 
