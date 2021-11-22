32b43fddf2d4094b6cc5885300c1c1dd0817e41b
hamis-juma <hamisjuma1@gmail.com>
2021-11-22 17:07:27 +0300

    updating workflow pending table

--- resources/views/system/workflow/count_summary.blade.php
+++ resources/views/system/workflow/count_summary.blade.php
@@ -5,22 +5,25 @@
             <div class="row">
                 @foreach($group_count as $value)
                     @if($value['count'])
-                        <div class="col-12 col-sm-3">
-                            <a class="module_summary" id="{!! $value['id'] !!}"  href="{{ url("/") . "/" . request()->route()->uri() }}?wf_module_id={{ $value['id'] }}"   @if(!empty
-                        ($value['description'])) title="{{ $value['description'] }}" data-toggle="popover" data-trigger="hover"
-                               data-placement="bottom" data-content="{{ $value['description'] }}" @endif >
-                                <div class="info-box bg-light">
-                                    <div class="info-box-content">
-                                        <span class="info-box-text text-center text-muted">{{ $value['name'] }}</span>
-                                        <span class="info-box-number text-center text-muted mb-0">{{ $value['count'] }}</span>
+                        <div class="col-4 col-sm-4 col-lg-3">
+                            <a id="{!! $value['id'] !!}"  href="{{ url("/") . "/" . request()->route()->uri() }}?wf_module_id={{ $value['id'] }}"   @if(!empty
+                        ($value['description'])) data-toggle="popover" data-trigger="hover"
+                               data-placement="bottom" data-content="{{ $value['description'] }}" @endif>
+                                <div class="card">
+                                    <div class="card-body text-center">
+                                        <div class="text-muted mb-0"> {{ $value['name'] }} [ {{ $value['count'] }} ] </div>
                                     </div>
                                 </div>
                             </a>
                         </div>
+
                     @endif
                 @endforeach
             </div>
         @endforeach
+
+
+
     </div>
 </div>
 
