d683bd35e2d56d8303d3ed112d48c8af82f25baa
hamis-juma <hamisjuma1@gmail.com>
2021-11-22 19:17:18 +0300

    Complete Requisition datatables

--- app/Http/Controllers/Web/Requisition/Datatables/RequisitionDatatables.php
+++ app/Http/Controllers/Web/Requisition/Datatables/RequisitionDatatables.php
@@ -7,14 +7,79 @@ use Yajra\DataTables\DataTables;
 trait RequisitionDatatables
 {
     /**
-     * get access user rejected
      * @return mixed
      * @throws \Exception
      */
-    public function processingDatatable()
+    public function AccessProcessingDatatable()
     {
-        return DataTables::of($this->requisitions->processingDatatable())
+        return DataTables::of($this->requisitions->getAccessProcessingDatatable())
             ->addIndexColumn()
+            ->editColumn('created_at', function ($query) {
+                return $query->created_at->toDateTimeString();
+            })
+            ->addColumn('amount', function ($query) {
+                return number_2_format($query->amount);
+            })
+            ->addColumn('action', function($query) {
+                return '<a href="'.route('requisition.show', $query->uuid).'">View</a>';
+            })
+            ->rawColumns(['action'])
+            ->make(true);
+    }
+
+    /**
+     * @return mixed
+     * @throws \Exception
+     */
+    public function AccessRejectedDatatable()
+    {
+        return DataTables::of($this->requisitions->getAccessRejectedDatatable())
+            ->addIndexColumn()
+            ->editColumn('created_at', function ($query) {
+                return $query->created_at->toDateTimeString();
+            })
+            ->addColumn('amount', function ($query) {
+                return number_2_format($query->amount);
+            })
+            ->addColumn('action', function($query) {
+                return '<a href="'.route('requisition.show', $query->uuid).'">View</a>';
+            })
+            ->rawColumns(['action'])
+            ->make(true);
+    }
+
+    /**
+     * @return mixed
+     * @throws \Exception
+     */
+    public function AccessApprovedDatatable()
+    {
+        return DataTables::of($this->requisitions->getAccessProvedDatatable())
+            ->addIndexColumn()
+            ->editColumn('created_at', function ($query) {
+                return $query->created_at->toDateTimeString();
+            })
+            ->addColumn('amount', function ($query) {
+                return number_2_format($query->amount);
+            })
+            ->addColumn('action', function($query) {
+                return '<a href="'.route('requisition.show', $query->uuid).'">View</a>';
+            })
+            ->rawColumns(['action'])
+            ->make(true);
+    }
+
+    /**
+     * @return mixed
+     * @throws \Exception
+     */
+    public function AccessSavedDatatable()
+    {
+        return DataTables::of($this->requisitions->getAccessSavedDatatable())
+            ->addIndexColumn()
+            ->editColumn('created_at', function ($query) {
+                return $query->created_at->toDateTimeString();
+            })
             ->addColumn('amount', function ($query) {
                 return number_2_format($query->amount);
             })
--- resources/views/requisition/_parent/datatables/access/index.blade.php
+++ resources/views/requisition/_parent/datatables/access/index.blade.php
@@ -4,10 +4,10 @@
             <div class="tabs-menu1 ">
                 <!-- Tabs -->
                 <ul class="nav panel-tabs">
-                    <li class=""><a href="#tab6" class="active" data-toggle="tab">Requested <span class="badge badge-primary">20</span></a></li>
-                    <li><a href="#tab6" data-toggle="tab" class="">Rejected {{--<span class="badge badge-danger">5</span>--}}</a></li>
-                    <li><a href="#tab6" data-toggle="tab" class="">Approved {{--<span class="badge badge-danger">5</span>--}}</a></li>
-                    <li><a href="#tab6" data-toggle="tab" class="">Saved {{--<span class="badge badge-danger">5</span>--}}</a></li>
+                    <li class=""><a href="#processing" class="active" data-toggle="tab">Requested <span class="badge badge-primary">20</span></a></li>
+                    <li><a href="#rejected" data-toggle="tab" class="">Rejected {{--<span class="badge badge-danger">5</span>--}}</a></li>
+                    <li><a href="#approved" data-toggle="tab" class="">Approved {{--<span class="badge badge-danger">5</span>--}}</a></li>
+                    <li><a href="#saved" data-toggle="tab" class="">Saved {{--<span class="badge badge-danger">5</span>--}}</a></li>
                 </ul>
             </div>
 
@@ -18,41 +18,21 @@
             </div>
 
         </div>
+
         <div class="panel-body tabs-menu-body">
             <div class="tab-content">
-{{--                <div class="tab-pane active" id="tab5">--}}
-
-{{--                    <div class="card-body">--}}
-{{--                        <div class="table-responsive">--}}
-{{--                            <table id="active_users" class="table table-striped table-bordered" style="width:100%">--}}
-{{--                                <thead>--}}
-{{--                                <tr>--}}
-{{--                                    <th class="wd-15p">#</th>--}}
-{{--                                    <th class="wd-15p">First name</th>--}}
-{{--                                    <th class="wd-15p">Last name</th>--}}
-{{--                                    <th class="wd-20p">Email</th>--}}
-{{--                                    <th class="wd-20p">Position</th>--}}
-{{--                                    <th class="wd-15p">Region</th>--}}
-{{--                                    <th class="wd-10p">Projects</th>--}}
-{{--                                    <th class="wd-25p">Action</th>--}}
-{{--                                </tr>--}}
-{{--                                </thead>--}}
-{{--                            </table>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-
-{{--                </div>--}}
-                <div class="tab-pane active" id="tab6">
+                <div class="tab-pane active" id="processing">
 
                     <div class="card-body">
                         <div class="table-responsive">
-                            <table id="diactive_users" class="table table-striped table-bordered" style="width:100%">
+                            <table id="access_processing" class="table table-striped table-bordered" style="width:100%">
                                 <thead>
                                 <tr>
                                     <th class="wd-15p">#</th>
                                     <th class="wd-15p">NUMBER</th>
                                     <th class="wd-15p">TYPE</th>
-                                    <th class="wd-20p">ITEMS LIST</th>
+                                    <th class="wd-15p">PROJECT</th>
+                                    <th class="wd-15p">ACTIVITY</th>
                                     <th class="wd-25p">AMOUNT</th>
                                     <th class="wd-25p">APPLIED DATE</th>
                                     <th class="wd-25p">ACTION</th>
@@ -65,6 +45,88 @@
                 </div>
             </div>
         </div>
+
+        <div class="panel-body tabs-menu-body">
+            <div class="tab-content">
+                <div class="tab-pane" id="rejected">
+
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table id="access_rejected" class="table table-striped table-bordered" style="width:100%">
+                                <thead>
+                                <tr>
+                                    <th class="wd-15p">#</th>
+                                    <th class="wd-15p">NUMBER</th>
+                                    <th class="wd-15p">TYPE</th>
+                                    <th class="wd-15p">PROJECT</th>
+                                    <th class="wd-15p">ACTIVITY</th>
+                                    <th class="wd-25p">AMOUNT</th>
+                                    <th class="wd-25p">APPLIED DATE</th>
+                                    <th class="wd-25p">ACTION</th>
+                                </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="panel-body tabs-menu-body">
+            <div class="tab-content">
+                <div class="tab-pane" id="approved">
+
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table id="access_approved" class="table table-striped table-bordered" style="width:100%">
+                                <thead>
+                                <tr>
+                                    <th class="wd-15p">#</th>
+                                    <th class="wd-15p">NUMBER</th>
+                                    <th class="wd-15p">TYPE</th>
+                                    <th class="wd-15p">PROJECT</th>
+                                    <th class="wd-15p">ACTIVITY</th>
+                                    <th class="wd-25p">AMOUNT</th>
+                                    <th class="wd-25p">APPLIED DATE</th>
+                                    <th class="wd-25p">ACTION</th>
+                                </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="panel-body tabs-menu-body">
+            <div class="tab-content">
+                <div class="tab-pane" id="saved">
+
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table id="access_saved" class="table table-striped table-bordered" style="width:100%">
+                                <thead>
+                                <tr>
+                                    <th class="wd-15p">#</th>
+                                    <th class="wd-15p">NUMBER</th>
+                                    <th class="wd-15p">TYPE</th>
+                                    <th class="wd-15p">PROJECT</th>
+                                    <th class="wd-15p">ACTIVITY</th>
+                                    <th class="wd-25p">AMOUNT</th>
+                                    <th class="wd-25p">APPLIED DATE</th>
+                                    <th class="wd-25p">ACTION</th>
+                                </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
 
@@ -72,22 +134,79 @@
     <script>
         $(document).ready(function () {
 
-            $("#active_users").DataTable({
+            $("#access_processing").DataTable({
                 // processing: true,
                 // serverSide: true,
                 destroy: true,
                 retrieve: true,
                 "responsive": true,
                 "autoWidth": false,
-                ajax: '{{ route('user.datatable.active') }}',
+                ajax: '{{ route('requisition.datatable.access.processing') }}',
                 columns: [
                     { data: 'DT_RowIndex', name: 'DT_RowIndex','bSortable': false, 'aTargets': [0], 'bSearchable': false },
-                    { data: 'first_name', name: 'users.first_name', searchable: true},
-                    { data: 'last_name', name: 'users.last_name', searchable: true},
-                    { data: 'email', name: 'users.email', searchable: true},
-                    { data: 'designation', name: 'unit.name', searchable: true},
-                    { data: 'region', name: 'regions.name', searchable: true},
-                    { data: 'project_list', name: 'projects.title', searchable: true },
+                    { data: 'number', name: 'requisitions.number', searchable: true},
+                    { data: 'type_title', name: 'requisitions.title', searchable: true},
+                    { data: 'project_title', name: 'projects.title', searchable: true},
+                    { data: 'activity_title', name: 'activities.title', searchable: true},
+                    { data: 'amount', name: 'requisitions.amount', searchable: true},
+                    { data: 'created_at', name: 'created_at', searchable: true },
+                    { data: 'action', name: 'action', searchable: false },
+                ]
+            });
+            $("#access_rejected").DataTable({
+                // processing: true,
+                // serverSide: true,
+                destroy: true,
+                retrieve: true,
+                "responsive": true,
+                "autoWidth": false,
+                ajax: '{{ route('requisition.datatable.access.rejected') }}',
+                columns: [
+                    { data: 'DT_RowIndex', name: 'DT_RowIndex','bSortable': false, 'aTargets': [0], 'bSearchable': false },
+                    { data: 'number', name: 'requisitions.number', searchable: true},
+                    { data: 'type_title', name: 'requisitions.title', searchable: true},
+                    { data: 'project_title', name: 'projects.title', searchable: true},
+                    { data: 'activity_title', name: 'activities.title', searchable: true},
+                    { data: 'amount', name: 'requisitions.amount', searchable: true},
+                    { data: 'created_at', name: 'created_at', searchable: true },
+                    { data: 'action', name: 'action', searchable: false },
+                ]
+            });
+            $("#access_approved").DataTable({
+                // processing: true,
+                // serverSide: true,
+                destroy: true,
+                retrieve: true,
+                "responsive": true,
+                "autoWidth": false,
+                ajax: '{{ route('requisition.datatable.access.approved') }}',
+                columns: [
+                    { data: 'DT_RowIndex', name: 'DT_RowIndex','bSortable': false, 'aTargets': [0], 'bSearchable': false },
+                    { data: 'number', name: 'requisitions.number', searchable: true},
+                    { data: 'type_title', name: 'requisitions.title', searchable: true},
+                    { data: 'project_title', name: 'projects.title', searchable: true},
+                    { data: 'activity_title', name: 'activities.title', searchable: true},
+                    { data: 'amount', name: 'requisitions.amount', searchable: true},
+                    { data: 'created_at', name: 'created_at', searchable: true },
+                    { data: 'action', name: 'action', searchable: false },
+                ]
+            });
+            $("#access_saved").DataTable({
+                // processing: true,
+                // serverSide: true,
+                destroy: true,
+                retrieve: true,
+                "responsive": true,
+                "autoWidth": false,
+                ajax: '{{ route('requisition.datatable.access.saved') }}',
+                columns: [
+                    { data: 'DT_RowIndex', name: 'DT_RowIndex','bSortable': false, 'aTargets': [0], 'bSearchable': false },
+                    { data: 'number', name: 'requisitions.number', searchable: true},
+                    { data: 'type_title', name: 'requisitions.title', searchable: true},
+                    { data: 'project_title', name: 'projects.title', searchable: true},
+                    { data: 'activity_title', name: 'activities.title', searchable: true},
+                    { data: 'amount', name: 'requisitions.amount', searchable: true},
+                    { data: 'created_at', name: 'created_at', searchable: true },
                     { data: 'action', name: 'action', searchable: false },
                 ]
             });
