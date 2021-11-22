944df9d8b5b22273a1eaab2c28fbda737109d84a
Frank Omary <91878811+frankomary@users.noreply.github.com>
2021-11-22 17:19:28 +0300

    Update initiate.blade.php

--- resources/views/requisition/_parent/form/initiate.blade.php
+++ resources/views/requisition/_parent/form/initiate.blade.php
@@ -5,39 +5,52 @@
     <div class="row">
 
         <div class="card">
+            <div class="card-header" style="background-color: rgb(238, 241, 248)">
+                <div class="row text-center">
+                    <span class="col-12 text-center font-weight-bold">Equipments</span>
+                </div>
+
+                <div class="card-options ">
+                    <a href="#" class="card-options-collapse" data-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a>
+                </div>
+
+            </div>
 
             <div class="card-body">
                 <div class="row">
                     <div class="col-md-12">
                         <div class="table-responsive push">
                             <table class="table table-bordered table-hover">
+
+
                                 <thead>
                                 <tr class="">
                                     <th class="text-center" style="width: 5%">#</th>
-                                    <th style="width: 20%">Equipment</th>
+                                    <th style="width: 10%">Equipment</th>
                                     <th class="text-center" style="width: 10%">Equipment Type</th>
-                                    <th class="text-center"  style="width: 40%">Specification/description</th>
+                                    <th class="text-center"  style="width: 20%">Specification/description</th>
+                                    <th class="text-center"  style="width: 10%">Allocation</th>
+                                    <th class="text-center"  style="width: 20%">Description</th>
                                     <th class="text-right"  style="width: 20%">Quantity / Request Amount</th>
                                     <th class="text-right" style="width: 5%">Action</th>
                                 </tr>
                                 </thead>
 
+                                </div>
+
                                 <tbody>
                                 @foreach($items as $key => $item)
                                 <tr>
                                     <td class="text-center">{{ $key + 1 }}</td>
-                                    <td>{{ $item->equipment->title }}</td>
+                                    <td>{{ $item->equipment->title }}
+                                    </td>
                                     <td class="text-center">{{ $item->equipment->type->title }}</td>
                                     <td class="text-left">
                                         <div>{{ $item->equipment->specs }}</div>
-                                        <hr>
+                                    </td>
+                                    <td class="text-center">
                                         <div>
-                                            <label>Reason to procure</label>
-                                            <div>{{ $item->reason }}</div>
-                                        </div>
-                                        <hr>
-                                        <div>
-                                            <label>District ( allocation )</label>
+                                            <label>District:</label>
                                             <div>
                                                 @foreach($item->districts as $district)
                                                     {{ $district->name }},
@@ -45,15 +58,21 @@
                                             </div>
                                         </div>
                                     </td>
+                                    <td class="text-center">
+                                        <div>
+                                            <label>Reason to procure:</label>
+                                            <div>{{ $item->reason }}</div>
+                                        </div>
+                                    </td>
                                     <td class="text-right">
 
                                         <div>
-                                            <label>Quantity</label>
+                                            <label>Quantity:</label>
                                             <div>{{ $item->quantity }}</div>
                                         </div>
 
                                         <div>
-                                            <label>Amount</label>
+                                            <label>Amount:</label>
                                             <div>${{ number_2_format($item->amount) }}</div>
                                         </div>
 
@@ -67,22 +86,35 @@
                                 @endforeach
                                 </tbody>
                             </table>
+
                         </div>
                     </div>
-                </div>
+{{--                </div>--}}
             </div>
         </div>
 
     </div>
 
     <div class="row">
-        <div class="card">
+        <div class="card" >
+
+            <div class="card-header" style="background-color: rgb(238, 241, 248)">
+                <div class="row text-center">
+                    <span class="col-12 text-center font-weight-bold">Add Equipments</span>
+                </div>
+
+                <div class="card-options ">
+                    <a href="#" class="card-options-collapse" data-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a>
+                </div>
+
+            </div>
 
             <div class="card-body">
                 <div class="row">
                     <div class="col-md-12">
                         <div class="table-responsive push">
                             <table class="table table-bordered table-hover">
+{{--                                <table class="table table-hover">--}}
                                 <thead>
                                 <tr class="">
                                     <th class="text-center" style="width: 5%">#</th>
@@ -98,7 +130,14 @@
                                 {!! Form::open(['route' => ['requisition_type.store',$requisition], 'method' => 'POST']) !!}
                                 <tr>
                                     <td class="text-center"><span style="color: red"></span></td>
-                                    <td>{!! Form::select('equipment_id',$equipments,null,['class'=>'form-control', 'placeholder'=>'Select','required']) !!}</td>
+                                    <td>{!! Form::select('equipment_id',$equipments,null,['class'=>'form-control', 'placeholder'=>'Select','required']) !!}
+                                    <br>
+                                        <hr>
+                                        <div>
+                                    <div id="equipment_type"></div>
+                                    </div>
+
+                                    </td>
                                     <td class="text-center" id="equipment_type"></td>
                                     <td class="text-left">
                                         <div id="specs"></div>
