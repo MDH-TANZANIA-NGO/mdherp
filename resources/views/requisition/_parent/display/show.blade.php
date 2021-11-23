9e4e20175a3b7af0bbbc2ca037630c4bd36abfea
Frank Omary <91878811+frankomary@users.noreply.github.com>
2021-11-23 15:10:05 +0300

    Update show.blade.php

--- resources/views/requisition/_parent/display/show.blade.php
+++ resources/views/requisition/_parent/display/show.blade.php
@@ -1,10 +1,21 @@
 @extends('layouts.app')
 @section('content')
-    <div class="row">
-        <div class="col-12">
-            <p class="text-center" style="font-size: 18px"><b>{{ $requisition->number }}</b></p>
+
+
+    <div class="card-header text-center" style="background-color: rgb(238, 241, 248)">
+        <div class="row text-center">
+            <span class="col-12 text-center font-weight-bold"><b>Requisition No.  {{ $requisition->number }}</b></span>
         </div>
     </div>
+
+
+{{--    <div class="row">--}}
+{{--        <div class="col-12">--}}
+{{--            <p class="text-center" style="font-size: 18px"><b>{{ $requisition->number }}</b></p>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+
+
     <!-- start: page -->
     <div class="row">
         <div class="col-lg-12">
