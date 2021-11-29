a1277744f79f14ee4c84c992361f3f7909b0e790
hamis-juma <hamisjuma1@gmail.com>
2021-11-29 14:26:07 +0300

    working on display travelling cost on requisition

--- resources/views/requisition/Direct/travelling/show.blade.php
+++ resources/views/requisition/Direct/travelling/show.blade.php
@@ -3,35 +3,10 @@
 <div class="row">
     <div class="col-md-12">
         <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">REQUISITION SUMMARY</h3>
-            </div>
+{{--            <div class="card-header">--}}
+{{--                <h3 class="card-title">REQUISITION SUMMARY</h3>--}}
+{{--            </div>--}}
             <div class="card-body">
-{{--                @if($requisition->user_id==access()->id())--}}
-{{--                <div class="">--}}
-{{--                    <h4 class="mb-1">Hi <strong>{{ $requisition->user->full_name_formatted }}</strong>,</h4>--}}
-{{--                    You have requested Amount of <strong>{{$requisition->amount}}</strong> (TZS) for activity:--}}
-{{--                    <p>{{$requisition->activity->title}}</p>--}}
-{{--                </div>--}}
-{{--                @else--}}
-
-{{--                    <div class="">--}}
-{{--                        <p>{{$requisition->activity->title}}</p>--}}
-{{--                    </div>--}}
-{{--                @endif--}}
-
-{{--                <div class="dropdown-divider"></div>--}}
-{{--                <div class="row pt-4">--}}
-{{--                    <div class="col-lg-6 ">--}}
-{{--                        <address>--}}
-{{--                            <strong>Project name: </strong><span class="text-primary" >{{$requisition->project->title}}</span><br>--}}
-{{--                            <strong>Sub Program area:</strong>  <span class="text-primary" >{{$requisition->activity->subprogram->programArea->title}}</span><br>--}}
-{{--                            <strong>Numeric Output: </strong> <span class="text-primary" >200</span><br>--}}
-{{--                            <strong>Output unit:</strong> <span class="text-primary" >  Pieces</span><br>--}}
-{{--                        </address>--}}
-{{--                    </div>--}}
-
-{{--                </div>--}}
                 <div class="table-responsive push">
                     <table class="table table-bordered table-hover">
                         <tr class=" ">
@@ -42,20 +17,21 @@
                             <th class="text-right" style="width: 1%">Transportation</th>
                             <th class="text-right" style="width: 1%">Others</th>
                         </tr>
+
+                        @foreach($travelling_costs as $cost)
                         <tr>
                             <td>
                                 <p class="font-w600 mb-1">Elinipendo Mziray</p>
                                 <div class="text-muted">Data collection</div>
                                 <div class="nn" style="color: green"><i class="fe fe-map-pin"></i> Nzega</div>
                             </td>
-                            <td class="text-center">10</td>
+                            <td class="text-center">{{ $cost->no_days }}</td>
                             <td class="text-right">60,000</td>
-                            <td class="text-right">200,000</td>
-                            <td class="text-right">60,000</td>
-                            <td class="text-right">200,000</td>
+                            <td class="text-right">{{ $cost->accommodation }}</td>
+                            <td class="text-right">{{ $cost->transportation }}</td>
+                            <td class="text-right">{{ $cost->other_cost }}</td>
                         </tr>
-
-
+                        @endforeach
 
                         <tr>
                             <td colspan="5" class="font-w600 text-right">Total TZS</td>
