39fed9aba4419fd873106840a3a138eba16ec645
hamis-juma <hamisjuma1@gmail.com>
2021-12-03 14:20:04 +0300

    working on user

--- app/Http/Controllers/Web/System/RegionController.php
+++ app/Http/Controllers/Web/System/RegionController.php
@@ -32,8 +32,9 @@ class RegionController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function byActivity()
+    public function byActivity(Request $request)
     {
+        Log::alert($request->all());
         Log::warning(\request()->all());
         Log::warning(request()->all());
         $regions = $this->regions->getByActivity(request()->only('activity_id'));
