2a8043fec075a559b61012d60184354f208efd6e
Elinipendo Mziray <emziray@mdh.or.tz>
2021-12-06 09:10:27 +0300

    fleet to Fleet controller

--- routes/web/fleet/fleet.php
+++ routes/web/fleet/fleet.php
@@ -1,5 +1,5 @@
 <?php
-Route::group(['namespace' => 'fleet', 'middleware' => ['web', 'auth'], 'prefix' => 'fleet', 'as' => 'fleet.'], function () {
+Route::group(['namespace' => 'Fleet', 'middleware' => ['web', 'auth'], 'prefix' => 'fleet', 'as' => 'fleet.'], function () {
     Route::get('', 'FleetController@index')->name('index');
     Route::get('register','FleetController@create')->name('register');
     Route::post('store','FleetController@store')->name('store');
