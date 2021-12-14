1ac5dff297a890188038ca25325f2e75eeb69542
Elinipendo Mziray <emziray@mdh.or.tz>
2021-12-14 14:50:33 +0300

    working on description

--- resources/views/requisition/_parent/form/initiate.blade.php
+++ resources/views/requisition/_parent/form/initiate.blade.php
@@ -1,15 +1,86 @@
 @extends('layouts.app')
+
 @section('content')
 
-    <div class="row">
-        <div class="col-xl-12 col-lg-12 col-md-12">
-            {!! Form::open(['route' => ['requisition.store',$requisition],'class'=>'card']) !!}
-            <div class="form-group">
+    @if($items->count() > 0 or $travelling_costs->count() > 0 or $training_costs->count() > 0)
 
-            </div>
+
+    <div class="row mb-4">
+        <div class="col-12">
+
+            <a class="btn btn-primary float-right" href="{{ route('logout') }}"
+               onclick="event.preventDefault();if(confirm('Are you sure you want to send it for approval')){document.getElementById('submit_requisition_form').submit()}">
+
+                <i class="dropdown-icon mdi  mdi-logout-variant" style="color: #fff"></i> Submit for approval
+            </a>
+
+            <form id="submit_requisition_form" action="{{ route('requisition.submit',$requisition) }}" method="POST" class="d-none">
+                @csrf
+            </form>
+
 
         </div>
+    </div>
 
-    {!! Form::close() !!}
+    @endif
+
+    @switch($requisition->requisition_type_id)
+        @case(1)
+        @if($items->count() > 0)
+            @include('requisition.procurement.items.details')
+        @endif
+        @include('requisition.procurement.forms.create',['items'=>$requisition->items])
+
+        @break
+
+        @case(2)
+
+        @switch($requisition->requisition_type_category)
+            @case(1)
+            @include('requisition.Direct.travelling.index',['items' => $requisition->items])
+            @break
+
+            @case(2)
+            @include('requisition.Direct.training.index',['items' => $requisition->items])
+            @break
+        @endswitch
+
+        @break
+
+    @endswitch
 
 @endsection
+
+@push('after-scripts')
+    <script>
+        $(document).ready(function (){
+            let $equipment_id = $("select[name='equipment_id']");
+            let $equipment_type = $("#equipment_type");
+            let $specs = $("#specs");
+            let $requested_amount = $("input[name='requested_amount']");
+
+
+            $equipment_id.change(function (event){
+                event.preventDefault();
+                let $equipment = $(this).val();
+                $requested_amount.attr('min', '');
+                $requested_amount.attr('max', '');
+                $.get("{{ route('equipment.get_by_id') }}", { equipment_id: $equipment},
+                    function(data, status){
+                        if(data){
+                            $equipment_type.text(data.equipment_title)
+                            $specs.text(data.specs)
+                            $requested_amount.attr('placeholder', data.price_range_from +' - ' +data.price_range_to)
+                            $requested_amount.attr('min', data.price_range_from)
+                            $requested_amount.attr('max', data.price_range_to)
+                        }else{
+                            $equipment_type.text('');
+                            $specs.text('');
+                            $requested_amount.empty();
+                        }
+                });
+
+            });
+        });
+    </script>
+@endpush
--- resources/views/requisition/description/forms/create.blade.php
+++ resources/views/requisition/description/forms/create.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.app')
-<div class="row">
-    <div class="col-xl-12 col-lg-12 col-md-12">
-    {!! Form::open(['route' => ['requisition.store',$requisition],'class'=>'card']) !!}
-    <div class="form-group">
+@section('content')
 
-    </div>
+    <div class="row">
+        <div class="col-xl-12 col-lg-12 col-md-12">
+            {!! Form::open(['route' => ['requisition.store',$requisition],'class'=>'card']) !!}
+            <div class="form-group">
 
-    </div>
+            </div>
 
-{!! Form::close() !!}
+        </div>
+
+    {!! Form::close() !!}
+
+@endsection
