a65d8bd39640c63520812535e8ac6f642acb436c
williammmussa <eng.williammussa@gmail.com>
2021-12-14 21:23:45 +0300

    initial commit for the hr module

--- public/js/app.js
+++ public/js/app.js
@@ -37410,8 +37410,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/frank/Documents/Projects/mdh_erp/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/frank/Documents/Projects/mdh_erp/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/williammussa/Code/mdherp/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/williammussa/Code/mdherp/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
