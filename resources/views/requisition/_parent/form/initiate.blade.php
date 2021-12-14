597a3d5f13e8fc6cf577ee8d777e752735e3d8f1
Elinipendo Mziray <emziray@mdh.or.tz>
2021-12-14 14:45:27 +0300

    form modifications for description

--- resources/views/requisition/_parent/form/initiate.blade.php
+++ resources/views/requisition/_parent/form/initiate.blade.php
@@ -37,11 +37,11 @@
 
         @switch($requisition->requisition_type_category)
             @case(1)
-            @include('requisition.Direct.travelling.index',['items' => $requisition->items])
+            @include('requisition.description.forms.create',['items' => $requisition->items])
             @break
 
             @case(2)
-            @include('requisition.Direct.training.index',['items' => $requisition->items])
+            @include('requisition.description.forms.create',['items' => $requisition->items])
             @break
         @endswitch
 
--- resources/views/requisition/description/forms/create.blade.php
+++ resources/views/requisition/description/forms/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 <div class="row">
     <div class="col-xl-12 col-lg-12 col-md-12">
-    {!! Form::open(['route' => ['travelling.store',$requisition],'class'=>'card']) !!}
+    {!! Form::open(['route' => ['requisition.store',$requisition],'class'=>'card']) !!}
     <div class="form-group">
 
     </div>
