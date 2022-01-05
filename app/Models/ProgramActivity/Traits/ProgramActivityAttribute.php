e43cd53ad2209f55653bf4b832aca61f2c8b6108
Hamis Juma <hamisjuma1@gmail.com>
2022-01-05 07:17:03 +0300

    Adding Resource name to programActivityAttribute

--- app/Models/ProgramActivity/Traits/ProgramActivityAttribute.php
+++ app/Models/ProgramActivity/Traits/ProgramActivityAttribute.php
@@ -5,5 +5,10 @@ namespace App\Models\ProgramActivity\Traits;
 
 trait ProgramActivityAttribute
 {
-
+    public function getResourceNameAttribute()
+    {
+        return "<b>".$this->number."</b> <br>".
+            $this->user->full_name_formatted."<br>".
+            $this->user->designation_title;
+    }
 }
