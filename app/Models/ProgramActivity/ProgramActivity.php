436cb614ccf0dde98f9726201fcf0e54d948c0a4
Hamis Juma <hamisjuma1@gmail.com>
2022-01-05 07:14:55 +0300

    Adding Trait to the modal

--- app/Models/ProgramActivity/ProgramActivity.php
+++ app/Models/ProgramActivity/ProgramActivity.php
@@ -4,10 +4,11 @@ namespace App\Models\ProgramActivity;
 
 use App\Models\BaseModel;
 use App\Models\ProgramActivity\Traits\ProgramActivityRelationship;
+use App\Models\ProgramActivity\Traits\ProgramActivityAttribute;
 use Illuminate\Database\Eloquent\Model;
 
 class ProgramActivity extends BaseModel
 {
     //
-    use ProgramActivityRelationship;
+    use ProgramActivityRelationship, ProgramActivityAttribute;
 }
