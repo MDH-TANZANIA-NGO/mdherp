1da5bc621673c276264acce5561e68d88e156d5a
Elinipendo Mziray <emziray@mdh.or.tz>
2022-01-13 16:00:40 +0300

    retirements numbers

--- app/Repositories/Retirement/RetirementRepository.php
+++ app/Repositories/Retirement/RetirementRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories\Retirement;
 
 use App\Models\Requisition\Travelling\requisition_travelling_cost;
 use App\Models\Retirement\Retirement;
+use App\Models\SafariAdvance\SafariAdvance;
 use App\Repositories\BaseRepository;
 use Illuminate\Support\Facades\DB;
 
@@ -20,10 +21,10 @@ class RetirementRepository extends BaseRepository
 
     public function inputProcess($inputs)
     {
-       $safari_advance_id = safari_advance_id::query()->find($inputs['safari_advance_id']);
+       $safari_advance_id = SafariAdvance::all()->find($inputs['safari_advance_id']);
         return[
 
-            'user_id'=> $safari_advance_id->traveller_uid,
+            'user_id'=> access()->user()->id,
             'safari_advance_id' => $inputs['safari_advance_id'],
             'amount_requested'=>$safari_advance_id->amount_requested,
             'amount_paid'=>$safari_advance_id->amount_paid,
