51190848bec928a53cccb619f8ed16f69d93147f
juma purusa <jmpurusa@gmail.com>
2022-01-28 12:59:59 +0300

    Starting facility managment

---
+++ database/migrations/v105/2022_01_28_084312_add_column_postcode_on_wards_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnPostcodeOnWardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('wards', function (Blueprint $table) {
+            $table->string('postcode')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('wards', function (Blueprint $table) {
+            //
+        });
+    }
+}
