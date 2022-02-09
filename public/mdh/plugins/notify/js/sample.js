e4bd1360a80e10c73fc04e199129520046027d2a
Elinipendo Mziray <emziray@mdh.or.tz>
2022-02-09 11:46:06 +0300

    working on sweet alert

--- public/mdh/plugins/notify/js/sample.js
+++ public/mdh/plugins/notify/js/sample.js
@@ -32,7 +32,7 @@
 
 		 function not1(){
             notif({
-				msg: "<b>Success:</b> Well done Details Submitted Successfully",
+				msg: "<b>Success:</b> Mambo",
 				type: "success"
 			});
         }
