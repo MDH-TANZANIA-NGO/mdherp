b8438500e338d2f5e99b1b2ae8e79757c0ec4239
Elinipendo Mziray <emziray@mdh.or.tz>
2022-03-03 08:27:19 +0300

    conflits resolved

--- app/Http/Controllers/Web/Requisition/RequisitionController.php
+++ app/Http/Controllers/Web/Requisition/RequisitionController.php
@@ -173,6 +173,7 @@ class RequisitionController extends Controller
             ->with('items', $requisition->items)
             ->with('travelling_costs',$requisition->travellingCost)
             ->with('budget', Budget::query()->where('activity_id', $requisition->activity()->first()->id)->where('region_id', access()->user()->region_id)->first())
+            ->with('budget', Budget::query()->where('activity_id', $requisition->activity()->first()->id)->where('region_id', $requisition->user()->first()->region_id)->first())
             ->with('training_costs', $requisition->trainingCost)
             ->with('gofficer',$this->gofficer->getQuery()->get()->pluck('first_name', 'id'))
             ->with('grate',$this->grate->getQuery()->get()->pluck('amount','id'))
--- resources/views/programactivity/forms/create.blade.php
+++ resources/views/programactivity/forms/create.blade.php
@@ -5,6 +5,62 @@
     <button type="submit" class="btn btn-outline-info" style="margin-left:40%;" >Submit For Approval</button>
     {!! Form::close() !!}
         <div class="offer offer-info mb-6">
+    <button type="submit" class="btn btn-primary float-right" >Submit For Approval</button>
+    {!! Form::close() !!}
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12">
+        <div class="card">
+            <div class="card-header" style="background-color: rgb(238, 241, 248)">
+                <h3 class="card-title">Activity Schedule</h3>
+                <div class="card-options ">
+                    <a href="#" class="card-options-collapse" data-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a>
+                    <a href="#" class="card-options-remove" data-toggle="card-remove"><i class="fe fe-x"></i></a>
+                </div>
+            </div>
+
+            <div class="card-body">
+                {!! Form::open(['route' => ['training.updateSchedule',$requisition_training->uuid], 'method'=>'POST']) !!}
+
+                {{--    {!! Form::number('requisition_id', $requisition->id,['class' => 'form-control', 'required', 'hidden']) !!}--}}
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            {!! Form::label('from', __("Start Date"),['class'=>'form-label','required_asterik']) !!}
+                            <input type="date" min="{{ now()->toDateString('Y-m-d') }}" class="form-control" name="from" value="{{$requisition_training->start_date}}">
+                            {!! $errors->first('from', '<span class="badge badge-danger">:message</span>') !!}
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            {!! Form::label('to', __("End Date"),['class'=>'form-label','required_asterik']) !!}
+                            {!! Form::date('to',$requisition_training->end_date,['class' => 'form-control', 'placeholder' => '','required', 'id'=>'to']) !!}
+                            {!! $errors->first('to', '<span class="badge badge-danger">:message</span>') !!}
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            {!! Form::label('activity_location', __("Activity Location"),['class'=>'form-label','required_asterik']) !!}
+                            {!! Form::select('district_id',$district,$requisition_training->district_id,['class' => 'form-control select2-show-search','required']) !!}
+                            {!! $errors->first('district_id', '<span class="badge badge-danger">:message</span>') !!}
+                        </div>
+                    </div>
+                </div>
+
+
+                <button type="submit" class="btn btn-primary" style="margin-left: 40%">Save changes</button>
+
+                {!! Form::close() !!}
+            </div>
+        </div>
+
+        </div>
+    </div>
+
+
+{{--        <div class="offer offer-info mb-6">
             <div class="shape">
                 <div class="shape-text">
                 </div>
@@ -27,6 +83,7 @@
                 </p>
             </div>
         </div>
+        </div>--}}
 
 
     <div class="row">
@@ -39,6 +96,7 @@
                 <div class="card-body">
                     <div class="table-responsive">
                         <table class="table card-table table-vcenter text-nowrap">
+                        <table id="example" class="table table-striped table-bordered" style="width:100%">
                             <thead >
                             <tr>
 
@@ -61,7 +119,7 @@
                                 <td>{{$training_costs->user->phone}}</td>
                                 <td>{{number_2_format($training_costs->perdiem_total_amount)}}</td>
                                 <td>{{number_2_format($training_costs->transportation)}}</td>
-                                <td>{{number_2_format($training_costs->other_cost)}}</td>
+                                <td>{{number_2_format($training_costs->other_cost)}} <span class="text-success">{{$training_costs->others_description}}</span></td>
                                 <td>{{number_2_format($training_costs->total_amount)}}</td>
 
 
