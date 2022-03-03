142cdccbfd4442c013b54d4ab6880813b4611004
Elinipendo Mziray <emziray@mdh.or.tz>
2022-03-03 17:54:37 +0300

    working on program activity report

--- app/Http/Controllers/Web/ProgramActivity/ProgramActivityReportController.php
+++ app/Http/Controllers/Web/ProgramActivity/ProgramActivityReportController.php
@@ -3,14 +3,28 @@
 namespace App\Http\Controllers\Web\ProgramActivity;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Web\ProgramActivity\Datatable\ProgramActivityDatatable;
+use App\Models\ProgramActivity\ProgramActivity;
+use App\Models\ProgramActivity\ProgramActivityReport;
+use App\Repositories\ProgramActivity\ProgramActivityRepository;
 use Illuminate\Http\Request;
 
 class ProgramActivityReportController extends Controller
 {
     //
+    use ProgramActivityDatatable;
+
+    protected $program_activities;
+
+    public function __construct(){
+
+        $this->program_activities =  (new ProgramActivityRepository());
+    }
 
     public function index(){
 
+        return view('programactivity.datatable.reports.all')
+            ->with('program_activities', $this->program_activities);
     }
 
     public function create(){
---
+++ resources/views/programactivity/datatable/reports/all.blade.php
@@ -0,0 +1,218 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="card-body p-6">
+    <div class="panel panel-primary">
+
+        <div class=" tab-menu-heading card-header" style="background-color: rgb(238, 241, 248)">
+            <div class="tabs-menu1 ">
+                <!-- Tabs -->
+                <ul class="nav panel-tabs">
+                    <li class=""><a href="#processing" class="active" data-toggle="tab">Onprocess <span class="badge badge-warning">{{ $program_activities->getAccessProcessingDatatable()->count() }}</span></a></li>
+                    <li><a href="#rejected" data-toggle="tab" class="">Returned <span class="badge badge-danger">{{ $program_activities->getAccessRejectedDatatable()->count() }}</span></a></li>
+                    <li><a href="#approved" data-toggle="tab" class="">Approved <span class="badge badge-success">{{ $program_activities->getAccessProvedDatatable()->count() }}</span></a></li>
+                    <li><a href="#saved" data-toggle="tab" class="">Saved <span class="badge badge-default">{{ $program_activities->getAccessSavedDatatable()->count() }}</span> </a></li>
+                </ul>
+            </div>
+
+
+            <div class="page-rightheader ml-auto d-lg-flex d-non pull-right">
+
+                <div class="btn-group mb-0">
+
+
+                    <a href="{{ route('programactivity.initiate') }}"> <i class="fa fa-plus mr-2"></i>Initiate Report</a>
+                </div>
+
+
+
+            </div>
+
+
+        </div>
+
+        <div class="panel-body tabs-menu-body" style="background-color:#FFFFFF">
+            <div class="tab-content">
+                <div class="tab-pane active" id="processing">
+
+                    <div class="card-body">
+
+                        <div class="table-responsive">
+                            <table id="access_processing" class="table table-striped table-bordered" style="width:100%">
+                                <thead>
+                                <tr>
+                                    <th class="wd-15p">#</th>
+                                    <th class="wd-15p">REPORT NUMBER</th>
+                                    <th class="wd-25p">ACTIVITY NUMBER</th>
+                                    <th class="wd-25p">REPORT TYPE</th>
+                                    <th class="wd-25p">CREATED ON</th>
+                                    <th class="wd-25p">ACTION</th>
+                                </tr>
+                                </thead>
+                            </table>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="tab-pane" id="saved">
+
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table id="access_saved" class="table table-striped table-bordered" style="width:100%">
+                                <thead>
+                                <tr>
+                                    <th class="wd-15p">#</th>
+                                    <th class="wd-15p">REPORT NUMBER</th>
+                                    <th class="wd-25p">ACTIVITY NUMBER</th>
+                                    <th class="wd-25p">REPORT TYPE</th>
+                                    <th class="wd-25p">CREATED ON</th>
+                                    <th class="wd-25p">ACTION</th>
+                                </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="tab-pane" id="approved">
+
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table id="access_approved" class="table table-striped table-bordered" style="width:100%">
+                                <thead>
+                                <tr>
+                                    <th class="wd-15p">#</th>
+                                    <th class="wd-15p">REPORT NUMBER</th>
+                                    <th class="wd-25p">ACTIVITY NUMBER</th>
+                                    <th class="wd-25p">REPORT TYPE</th>
+                                    <th class="wd-25p">CREATED ON</th>
+                                    <th class="wd-25p">ACTION</th>
+                                </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="tab-pane" id="rejected">
+
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table id="access_rejected" class="table table-striped table-bordered" style="width:100%">
+                                <thead>
+                                <tr>
+                                    <th class="wd-15p">#</th>
+                                    <th class="wd-15p">REPORT NUMBER</th>
+                                    <th class="wd-25p">ACTIVITY NUMBER</th>
+                                    <th class="wd-25p">REPORT TYPE</th>
+                                    <th class="wd-25p">CREATED ON</th>
+                                    <th class="wd-25p">ACTION</th>
+                                </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+
+                </div>
+
+
+            </div>
+        </div>
+
+
+
+    </div>
+</div>
+@endsection
+@push('after-scripts')
+    <script>
+        $(document).ready(function () {
+
+            $("#access_processing").DataTable({
+                // processing: true,
+                // serverSide: true,
+                destroy: true,
+                retrieve: true,
+                "responsive": true,
+                "autoWidth": false,
+                ajax: '{{ route('programactivity.datatable.access.processing') }}',
+                columns: [
+                    { data: 'DT_RowIndex', name: 'DT_RowIndex','bSortable': false, 'aTargets': [0], 'bSearchable': false },
+                    { data: 'number', name: 'safari_advances.number', searchable: true},
+                    { data: 'amount_requested', name: 'safari_advances.amount_requested', searchable: true},
+                    { data: 'amount_requested', name: 'safari_advances.amount_requested', searchable: true},
+                    // { data: 'project_title', name: 'projects.title', searchable: true},
+                    // { data: 'activity_title', name: 'activities.title', searchable: true},
+                    // { data: 'amount', name: 'requisitions.amount', searchable: true},
+                    { data: 'created_at', name: 'created_at', searchable: true },
+                    { data: 'action', name: 'action', searchable: false },
+                ]
+            });
+            $("#access_rejected").DataTable({
+                // processing: true,
+                // serverSide: true,
+                destroy: true,
+                retrieve: true,
+                "responsive": true,
+                "autoWidth": false,
+                ajax: '{{ route('programactivity.datatable.access.rejected') }}',
+                columns: [
+                    { data: 'DT_RowIndex', name: 'DT_RowIndex','bSortable': false, 'aTargets': [0], 'bSearchable': false },
+                    { data: 'number', name: 'program_activities.number', searchable: true},
+                    { data: 'amount_requested', name: 'program_activities.amount_requested', searchable: true},
+                    { data: 'amount_requested', name: 'safari_advances.amount_requested', searchable: true},
+                    // { data: 'project_title', name: 'projects.title', searchable: true},
+                    // { data: 'activity_title', name: 'activities.title', searchable: true},
+                    // { data: 'amount', name: 'requisitions.amount', searchable: true},
+                    { data: 'created_at', name: 'created_at', searchable: true },
+                    { data: 'action', name: 'action', searchable: false },
+                ]
+            });
+            $("#access_approved").DataTable({
+                // processing: true,
+                // serverSide: true,
+                destroy: true,
+                retrieve: true,
+                "responsive": true,
+                "autoWidth": false,
+                ajax: '{{ route('programactivity.datatable.access.approved') }}',
+                columns: [
+                    { data: 'DT_RowIndex', name: 'DT_RowIndex','bSortable': false, 'aTargets': [0], 'bSearchable': false },
+                    { data: 'number', name: 'program_activities.number', searchable: true},
+                    { data: 'amount_requested', name: 'program_activities.amount_requested', searchable: true},
+                    { data: 'amount_requested', name: 'safari_advances.amount_requested', searchable: true},
+                    // { data: 'project_title', name: 'projects.title', searchable: true},
+                    // { data: 'activity_title', name: 'activities.title', searchable: true},
+                    // { data: 'amount', name: 'requisitions.amount', searchable: true},
+                    { data: 'created_at', name: 'created_at', searchable: true },
+                    { data: 'action', name: 'action', searchable: false },
+                ]
+            });
+            $("#access_saved").DataTable({
+                // processing: true,
+                // serverSide: true,
+                destroy: true,
+                retrieve: true,
+                "responsive": true,
+                "autoWidth": false,
+                ajax: '{{ route('programactivity.datatable.access.saved') }}',
+                columns: [
+                    { data: 'DT_RowIndex', name: 'DT_RowIndex','bSortable': false, 'aTargets': [0], 'bSearchable': false },
+                    { data: 'number', name: 'program_activities.number', searchable: true},
+                    { data: 'amount_requested', name: 'program_activities.amount_requested', searchable: true},
+                    { data: 'amount_requested', name: 'safari_advances.amount_requested', searchable: true},
+                    // { data: 'project_title', name: 'projects.title', searchable: true},
+                    // { data: 'activity_title', name: 'activities.title', searchable: true},
+                    // { data: 'amount_paid', name: 'safari_advances.amount_paid', searchable: true},
+                    { data: 'created_at', name: 'created_at', searchable: true },
+                    { data: 'action', name: 'action', searchable: false },
+                ]
+            });
+
+        })
+    </script>
+@endpush
