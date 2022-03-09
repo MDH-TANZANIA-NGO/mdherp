076fb38b07556942528816167995fa66c5f59bc8
hamis-juma <hamisjuma1@gmail.com>
2022-03-09 13:08:33 +0300

    Currency converter completed

--- routes/web/rate/rate.php
+++ routes/web/rate/rate.php
@@ -1,11 +1,12 @@
 <?php
 Route::group(['namespace' => 'Rate', 'middleware' => ['web', 'auth'], 'prefix' => 'rates', 'as' => 'rate.'], function () {
     Route::get('', 'RateController@index')->name('index');
-    Route::get('submitted', 'RateController@show')->name('show');
-    Route::get('submitted', 'RateController@store')->name('store');
-    Route::get('approved', 'RateController@update')->name('update');
-});
+    Route::get('{rate}/show', 'RateController@show')->name('show');
+    Route::post('store', 'RateController@store')->name('store');
+    Route::put('{rate}/update', 'RateController@update')->name('update');
 
-Route::group(['prefix' => 'datatables', 'as' => 'leave_report.'], function () {
-    Route::get('', 'RateController@allDatatable')->name('all');
+    Route::group(['prefix' => 'datatables', 'as' => 'datatable.'], function () {
+        Route::get('all', 'RateController@allDatatable')->name('all');
+    });
+
 });
