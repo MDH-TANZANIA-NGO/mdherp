605c1051a6ac4312a9becdf9e8e85caa6e12ed2e
William Mussa <eng.williammussa>
2022-03-10 08:38:29 +0300

    leave modification

--- app/Http/Controllers/Web/Leave/LeaveController.php
+++ app/Http/Controllers/Web/Leave/LeaveController.php
@@ -130,7 +130,7 @@ class LeaveController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Leave $leave)
     {
         //
     }
@@ -142,7 +142,7 @@ class LeaveController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Leave $leave)
     {
         //
     }
