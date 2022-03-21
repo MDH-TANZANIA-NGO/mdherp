683bb06633a20ff2bb49f7c7514f498431d020f6
Elinipendo Mziray <emziray@mdh.or.tz>
2022-03-21 14:56:29 +0300

    Update UsersImport.php

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -5,8 +5,9 @@ namespace App\Imports;
 use App\Models\Auth\User;
 //use App\Models\User;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class UsersImport implements ToModel
+class UsersImport implements ToModel, WithHeadingRow
 {
     /**
     * @param array $row
