4165ed992c58a2c85feb7d7e62bc3775be2eb77b
Elinipendo Mziray <emziray@mdh.or.tz>
2022-05-05 09:27:54 +0300

    Update index.blade.php

--- resources/views/workspace/index.blade.php
+++ resources/views/workspace/index.blade.php
@@ -43,7 +43,7 @@
 
         </div>
 
-        <div class="col-4 col-sm-4 col-lg-3">
+        {{--<div class="col-4 col-sm-4 col-lg-3">
             <a href="{{route('stock.index')}}">
                 <div class="card">
                     <div class="card-body text-center">
@@ -65,7 +65,7 @@
                 </div>
             </a>
 
-        </div>
+        </div>--}}
 
         <div class="col-4 col-sm-4 col-lg-3">
             <a href="{{ route('retirement.index') }}">
