566dbe5a454461c00b4da9b51a4fff026e1c056e
Elinipendo Mziray <emziray@mdh.or.tz>
2022-05-16 04:53:06 +0300

    done

--- app/Http/Controllers/Web/Finance/FinanceActivityController.php
+++ app/Http/Controllers/Web/Finance/FinanceActivityController.php
@@ -486,3 +486,5 @@ class FinanceActivityController extends Controller
     }
 
 }
+
+
--- resources/views/requisition/Direct/training/index.blade.php
+++ resources/views/requisition/Direct/training/index.blade.php
@@ -1,21 +1,21 @@
 
-            <div class="tags">
+{{--            <div class="tags">--}}
 
-                <div class="tag">
-                    Available Fund
-                    <span class="tag-addon tag-success">
-                        @if($requisition->fundChecker == null)
-                            {{number_2_format($requisition->budget->actual_amount)}}
-                        @else
-                            {{number_2_format($requisition->fundChecker->actual_amount)}}
-                        @endif
-                    </span>
-                </div>
-                <span class="tag tag-dark">
-														Total amount requested
-														<span class="tag-addon tag-warning">{{number_2_format($requisition->amount)}}</span>
-													</span>
-            </div>
+{{--                <div class="tag">--}}
+{{--                    Available Fund--}}
+{{--                    <span class="tag-addon tag-success">--}}
+{{--                        @if($requisition->fundChecker == null)--}}
+{{--                            {{number_2_format($requisition->budget->actual_amount)}}--}}
+{{--                        @else--}}
+{{--                            {{number_2_format($requisition->fundChecker->actual_amount)}}--}}
+{{--                        @endif--}}
+{{--                    </span>--}}
+{{--                </div>--}}
+{{--                <span class="tag tag-dark">--}}
+{{--														Total amount requested--}}
+{{--														<span class="tag-addon tag-warning">{{number_2_format($requisition->amount)}}</span>--}}
+{{--													</span>--}}
+{{--            </div>--}}
      <br>
 
 @if($requisition->training->count() >0)
