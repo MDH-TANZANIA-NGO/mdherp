4d0a4dbb3c4b47c08efafb57f99717b870ec2bd7
Hamis Juma <hamisjuma1@gmail.com>
2022-05-19 12:53:37 +0300

    Design Print PDF

--- resources/views/requisition/_parent/display/show.blade.php
+++ resources/views/requisition/_parent/display/show.blade.php
@@ -13,6 +13,7 @@
     <!-- start: page -->
     <div class="row mb-2">
         <div class="col-lg-12">
+
             @include('includes.workflow.workflow_track', ['current_wf_track' => $current_wf_track])
         </div>
     </div>
