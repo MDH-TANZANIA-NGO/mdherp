2e782b2b6f5cee04648ad523ea7abe7aaa6256ca
nkayogx <47517846+nkayogx@users.noreply.github.com>
2022-05-23 12:26:52 +0300

    Bulk workplan upload : ignored duplicates workplans
    Ignored ( duplicate, already exist ) workplan while upload

--- resources/views/project/activity/index.blade.php
+++ resources/views/project/activity/index.blade.php
@@ -4,9 +4,31 @@
         <span class="col-12 text-center font-weight-bold">Activites</span>
     </div> --}}
     @include('project.activity.form.create')
+    @if(Session::has('importedRows'))
+        <div class="col-12">
+            <div class="alert alert-success text-light alert-dismissible fade show" role="alert">
+                {{ Session::get('importedRows') }} Record(s) Imported  
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>            
+            </div>
+        </div>
+    @endif
+
+    @if(Session::has('importedDuplicates'))
+        <div class="col-12">
+            <div class="alert alert-danger text-light alert-dismissible fade show" role="alert">
+                {{ Session::get('importedDuplicates') }} Record(s) Already Exist  
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>            
+            </div>
+        </div>
+    @endif
+    
 
     @include('project.activity.form.bulk_upload')
-
+  
     {{-- <div class="row  mb-3">
         <span class="col-12 text-center font-weight-bold">List of Activities</span>
     </div> --}}
