b0f2c54ec7475eb5ef8262c9acb1e60d033581f4
Elinipendo Mziray <emziray@mdh.or.tz>
2022-05-31 15:31:03 +0300

    Update index.blade.php

---
+++ database/seeds/v100/wardsSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class wardsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
