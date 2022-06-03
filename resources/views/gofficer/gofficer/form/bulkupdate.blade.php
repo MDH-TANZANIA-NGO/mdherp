299c5b28e8f164edeb4f360dce35e697d2730959
Elinipendo Mziray <emziray@mdh.or.tz>
2022-06-03 22:02:38 +0300

    Update bulkupdate.blade.php

--- resources/views/gofficer/gofficer/form/bulkupdate.blade.php
+++ resources/views/gofficer/gofficer/form/bulkupdate.blade.php
@@ -1,46 +1,69 @@
 @extends('layouts.app')
 @section('content')
-<div class="col-lg-12 col-md-12">
-    {!! Form::open(['route' => 'g_officer.filter']) !!}
 
-    <div class="row" style="margin-left: 25%" >
-        <div class="col-md-4">
-            <div class="form-group ">
-                {!! Form::label('region', __("label.region"),['class'=>'form-label','required_asterik']) !!}
-                {!! Form::select('region', $regions, null, ['class' =>'form-control select2-show-search', 'placeholder' => __('label.select') , 'aria-describedby' => '', 'required', 'style'=>'width:100%']) !!}
-                {!! $errors->first('region', '<span class="badge badge-danger">:message</span>') !!}
+
+
+
+<div class="row flex-lg-nowrap">
+    <div class="col-12 col-lg-3 mb-3">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Filter</h3>
+            </div>
+            <div class="card-body">
+
+                {!! Form::open(['route' => 'g_officer.filter']) !!}
+
+
+                        <div class="form-group ">
+                            {!! Form::label('region', __("label.region"),['class'=>'form-label','required_asterik']) !!}
+                            {!! Form::select('region', $regions, null, ['class' =>'form-control select2-show-search', 'placeholder' => __('label.select') , 'aria-describedby' => '', 'required', 'style'=>'width:100%']) !!}
+                            {!! $errors->first('region', '<span class="badge badge-danger">:message</span>') !!}
+                        </div>
+
+                        <div class="form-group ">
+                            {!! Form::label('Districts', __("Districts"),['class'=>'form-label','required_asterik']) !!}
+                            {{--                                        {!! Form::select('districts', $districts, null, ['class' =>'form-control select2-show-search', 'placeholder' => __('label.select') , 'aria-describedby' => '', 'required','style'=>'width:100%']) !!}--}}
+                            <select name="districts" class="form-control select2-show-search" style="width: 100%">
+                                <option value="">Select District</option>
+                            </select>
+                            {!! $errors->first('districts', '<span class="badge badge-danger">:message</span>') !!}
+                        </div>
+
+                    <button type="submit" class="btn btn-outline-success" style="margin-left: 30%"><i class="fa fa-file-excel-o"></i> Export Excel</button>
+
+
+                {!! Form::close() !!}
+
             </div>
         </div>
-        <div class="col-md-4">
-            <div class="form-group ">
-                {!! Form::label('Districts', __("Districts"),['class'=>'form-label','required_asterik']) !!}
-                {{--                                        {!! Form::select('districts', $districts, null, ['class' =>'form-control select2-show-search', 'placeholder' => __('label.select') , 'aria-describedby' => '', 'required','style'=>'width:100%']) !!}--}}
-                <select name="districts" class="form-control select2-show-search" style="width: 100%">
-                    <option value="">Select District</option>
-                </select>
-                {!! $errors->first('districts', '<span class="badge badge-danger">:message</span>') !!}
-            </div>
-        </div>
-        {{--        <div class="col-md-4">--}}
-        {{--            <div class="form-group ">--}}
-        {{--                {!! Form::label('Facilities', __("Facilities"),['class'=>'form-label','required_asterik']) !!}--}}
-        {{--                --}}{{--                                        {!! Form::select('facilities', $facilities, null, ['class' =>'form-control select2-show-search', 'placeholder' => __('label.select') , 'aria-describedby' => '', 'required','style'=>'width:100%']) !!}--}}
-        {{--                <select name="facilities" class="form-control select2-show-search" style="width: 100%">--}}
-        {{--                    <option value="">Select Facility</option>--}}
-        {{--                </select>--}}
-        {{--                {!! $errors->first('facilities', '<span class="badge badge-danger">:message</span>') !!}--}}
-        {{--            </div>--}}
-        {{--        </div>--}}
-        <button type="submit" class="btn btn-outline-success" style="margin-left: 30%"><i class="fa fa-file-excel-o"></i> Export Excel</button>
     </div>
+    <div class="col">
+        <div class="row flex-lg-nowrap">
+            <div class="col mb-3">
+                <div class="e-panel card">
+                    <div class="card-header">
+                        <h3 class="card-title">Users</h3>
+                        <div class="card-options ">
+                            <a href="#" class="card-options-collapse" data-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a>
+                            <a href="#" class="card-options-remove" data-toggle="card-remove"><i class="fe fe-x"></i></a>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="e-table">
+                            <div class="table-responsive table-lg mt-3">
+                                to be displayed
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-{!! Form::close() !!}
+        </div>
 
 
-<!--end col-->
-
+    </div>
 </div>
-
 @endsection
 @push('after-scripts')
     <script>
