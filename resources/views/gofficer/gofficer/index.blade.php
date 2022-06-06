0009a570179e54b029045180749048739c961a64
Elinipendo Mziray <emziray@mdh.or.tz>
2022-06-06 07:22:30 +0300

    Update index.blade.php

--- resources/views/gofficer/gofficer/index.blade.php
+++ resources/views/gofficer/gofficer/index.blade.php
@@ -4,14 +4,14 @@
 
     <div class="col-lg-12 col-md-12">
     {!! Form::open(['route' => 'g_officer.filter']) !!}
-
     <div class="row" style="margin-left: 25%" >
         <div class="col-md-4">
             <div class="form-group ">
                 {!! Form::label('region', __("label.region"),['class'=>'form-label','required_asterik']) !!}
                 {!! Form::select('region', $regions, null, ['class' =>'form-control select2-show-search', 'placeholder' => __('label.select') , 'aria-describedby' => '', 'required', 'style'=>'width:100%']) !!}
                 {!! $errors->first('region', '<span class="badge badge-danger">:message</span>') !!}
-
+            </div>
+        </div>
         <div class="col-md-4">
             <div class="form-group ">
                 {!! Form::label('Districts', __("Districts"),['class'=>'form-label','required_asterik']) !!}
