b160f44ddc875115a6ea4c76ffa56dfe9d59cbe2
Elinipendo Mziray <emziray@mdh.or.tz>
2022-06-06 14:54:21 +0300

    Update loginToken.php

--- routes/api/loginToken.php
+++ routes/api/loginToken.php
@@ -1,4 +1,4 @@
 <?php
-Route::group(['namespace' => 'UserLoginToken', 'middleware' => ['Api', 'auth'], 'prefix' => 'login_token', 'as' => 'login_token.'], function () {
+Route::group(['namespace' => 'UserLoginToken', 'middleware' => ['Api'], 'prefix' => 'login_token', 'as' => 'login_token.'], function () {
     Route::get('verifyToken', 'UserLoginTokenController@verifyToken')->name('verifyToken');
 });
