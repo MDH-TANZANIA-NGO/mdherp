99465c8b54fb7aaf291e887eda2b145e2a8498b2
Hamis Juma <hamisjuma1@gmail.com>
2022-06-23 12:36:42 +0300

    adding Goals and objectives on PrProbation

--- resources/views/HumanResource/PerformanceReview/saved.blade.php
+++ resources/views/HumanResource/PerformanceReview/saved.blade.php
@@ -1,8 +1,19 @@
 @extends('layouts.app')
 @section('content')
-    @if($pr_report->parent)
-        @include('HumanResource.PerformanceReview.includes.evaluation',['pr_report' => $pr_report->parent])
-    @else
-        @include('HumanResource.PerformanceReview.includes.report',['pr_report' => $pr_report])
-    @endif
+    @switch($pr_report->type->id)
+        @case(2)
+            @if($pr_report->parent)
+                @include('HumanResource.PerformanceReview.includes.evaluation',['pr_report' => $pr_report->parent])
+            @else
+                @include('HumanResource.PerformanceReview.includes.report',['pr_report' => $pr_report])
+            @endif
+        @break
+        @case(1)
+            @if($pr_report->parent)
+                @include('HumanResource.PerformanceReview.includes.evaluation',['pr_report' => $pr_report->parent])
+            @else
+                @include('HumanResource.PerformanceReview.includes.probation_report',['pr_report' => $pr_report])
+            @endif
+        @break
+    @endswitch
 @endsection
