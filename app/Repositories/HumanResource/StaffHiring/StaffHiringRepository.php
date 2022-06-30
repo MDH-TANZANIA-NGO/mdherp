91f573025863d4419402341c8787b68a62f4546c
Frank Omary <91878811+frankomary@users.noreply.github.com>
2022-06-30 13:56:23 +0300

    Update StaffHiringRepository.php

--- app/Repositories/HumanResource/StaffHiring/StaffHiringRepository.php
+++ app/Repositories/HumanResource/StaffHiring/StaffHiringRepository.php
@@ -4,12 +4,8 @@ namespace App\Repositories\HumanResource\Interview;
 
 use App\Exceptions\GeneralException;
 use App\Models\Auth\User;
-use App\Models\Budget\FiscalYear;
-use App\Models\HumanResource\Interview\Interview;
 use App\Models\HumanResource\StaffHiring\StaffHiring;
-use App\Notifications\Workflow\WorkflowNotification;
 use App\Repositories\BaseRepository;
-use App\Services\Generator\Number;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 
@@ -20,9 +16,22 @@ class StaffHiringRepository extends BaseRepository
     public function store($input)
     {
         return DB::transaction(function () use($input){
-            $input['candidate_name'] = 'candidate_name';
-
-            return $this->query()->create($input);
+            $inset = [
+                'candidate_name' => $input['candidate_name'],
+                'know_period' => $input['know_period'],
+                'know_capacity' => $input['know_capacity'],
+                'relate' => $input['relate'],
+                'rate_proffesional' => $input['rate_proffesional'],
+                'rate_commitment' => $input['rate_commitment'],
+                'rate_knowledge' => $input['rate_knowledge'],
+                'rate_trust' => $input['rate_trust'],
+                'rate_deadlines' => $input['rate_deadlines'],
+                'rate_relationship_with_others' => $input['rate_relationship_with_others'],
+                'general_opinion' => $input['general_opinion'],
+                'ref_name' => $input['ref_name'],
+                'ref_email' => $input['ref_email'],
+            ];
+            return $this->query()->create($inset);
         });
     }
 
