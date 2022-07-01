c3842a3a5625b9a7869a485ec0fcd7f3c4b13874
Elinipendo Mziray <emziray@mdh.or.tz>
2022-07-01 15:01:30 +0300

    change view to redirect rout to job managment

--- resources/views/workspace/index.blade.php
+++ resources/views/workspace/index.blade.php
@@ -126,7 +126,7 @@
 
     @permission('hire_requisition')
         <div class="col-4 col-sm-4 col-lg-3">
-            <a href="{{ route('hirerequisition.index') }}">
+            <a href="{{ route('hirerequisition.list') }}">
                 <div class="card">
                     <div class="card-body text-center">
                         <div class="h2 m-0"><i class="fa fa-child text-primary"></i></div>
