e4c8991c99d3e026e7eff2912a74cc485a9159b3
Frank Omary <91878811+frankomary@users.noreply.github.com>
2022-07-02 12:38:30 +0300

    Update new.blade.php

--- resources/views/HumanResource/StaffHiring/form/new.blade.php
+++ resources/views/HumanResource/StaffHiring/form/new.blade.php
@@ -1,8 +1,8 @@
 <div class="row">
     <div class="card">
         <div class="card-header" style="background-color: rgb(238, 241, 248)">
-            <h3 class="card-title">MDH
-            </h3>
+            <h3 class="card-title">MDH</h3> &nbsp;
+            <h4 class="card-title">Employee Reference Report Form (To be completed by a given Referee/ Employer)</h4>
         </div>
         <div class="card-body">
             {!! Form::open(['route' => 'refferenceform.store']) !!}
@@ -12,78 +12,78 @@
                     <div  class="col-md-4">
                         <div class="form-group">
                             <label class="form-label">Referring To:</label>
-                            <input type="text" name="candidate_name" class="form-control" placeholder="Jane Doe" >
+                            <input type="text" name="candidate_name" class="form-control" placeholder="Jane Doe" disabled>
                         </div>
                     </div>
                 </div>
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">Employee Reference Report Form (To be completed by a given Referee/ Employer)</h3>
-                        <div class="card-options ">
-{{--                            <a href="#" class="card-options-collapse" data-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a>--}}
+
+                    <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label class="form-label">For how long have you known him/her?</label>
+                            <input type="text" name="know_period" class="form-control" placeholder="" required>
                         </div>
                     </div>
-                    <div class="table-responsive">
-                        <table class="table card-table table-vcenter text-nowrap">
-                            <thead>
-                            <tr>
-                                <th>Item No.</th>
-                                <th>KEY AREA</th>
-                                <th>RESPONSE</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>For how long have you known him/her?</td>
-                                <td><input type="text" name="know_period" class="form-control" placeholder="" required></td>
-
-                            </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>In what capacity have you known him/her?</td>
-                                <td><input type="text" name="know_capacity" class="form-control" placeholder="" required></td>
-
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td>Is he/she related to you? If “yes” how?</td>
-                                <td><textarea class="form-control" name="relate" rows="2" placeholder="text here.."></textarea></td>
-
-                            </tr>
-                            <tr>
-                                <th scope="row">4</th>
-                                <td colspan="2">
-                                    <table>
-                                        <tbody>
-                                        <tr>
-                                            <td>How would you rate the candidate in terms of:</td>
-                                        </tr>
-
-                                        <tr><td style="width: 50%" >a) Professional conduct</td><td> <input type="text" name="rate_proffesional" class="form-control" placeholder="" required ></td></tr>
-                                        <tr><td style="width: 50%">b) Commitment to work</td><td><input type="text" name="rate_commitment" class="form-control" placeholder="" required></td></tr>
-                                        <tr><td style="width: 50%">c) Knowledge of work</td><td><input type="text" name="rate_knowledge" class="form-control" placeholder="" required></td></tr>
-                                        <tr><td style="width: 50%">d) Trustworthy</td><td><input type="text" name="rate_trust" class="form-control" placeholder="" required></td></tr>
-                                        <tr><td style="width: 50%">e) Meeting dead lines</td><td><input type="text" name="rate_deadlines" class="form-control" placeholder="" required></td></tr>
-                                        <tr><td style="width: 50%">f) Relationship with other workers</td><td><input type="text" name="rate_relationship_with_others" class="form-control" placeholder="" required></td></tr>
-                                        </tbody>
-                                    </table>
-                                </td>
-
-
-
-                            </tr>
-                            <tr>
-                                <th scope="row">5</th>
-                                <td>Please, give your general/specific opinion on his/her employability.</td>
-                                <td><textarea class="form-control" name="general_opinion" rows="2" placeholder="text here.."></textarea></td>
-
-                            </tr>
-                            </tbody>
-                        </table>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label class="form-label">In what capacity have you known him/her?</label>
+                            <input type="text" name="know_capacity" class="form-control" placeholder="" required>
+                        </div>
                     </div>
-                    <!-- table-responsive -->
+                    <div class="col-md-4">
+                            <div class="form-group">
+                                <label class="form-label">Is he/she related to you? If “yes” how?</label>
+                                <textarea class="form-control" name="relate" rows="2" placeholder="text here.."></textarea>
+                            </div>
+                    </div>
                 </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <label class="form-label">How would you rate the candidate in terms of:</label>
+                    </div>
+                </div>
+                &nbsp;
+                <div class="row">
+                    <div class="col-md-4">
+                        <label class="form-label">a) Professional conduct</label>
+                        <input type="text" name="rate_proffesional" class="form-control" placeholder="" required >
+                    </div>
+                    <div class="col-md-4">
+                        <label class="form-label">b) Commitment to work</label>
+                        <input type="text" name="rate_commitment" class="form-control" placeholder="" required>
+                    </div>
+                    <div class="col-md-4">
+                        <label class="form-label">c) Knowledge of work</label>
+                        <input type="text" name="rate_knowledge" class="form-control" placeholder="" required>
+                    </div>
+                </div>
+                &nbsp;
+                <div class="row">
+                    <div class="col-md-4">
+                        <label class="form-label">d) Trustworthy</label>
+                        <input type="text" name="rate_trust" class="form-control" placeholder="" required>
+                    </div>
+                    <div class="col-md-4">
+                        <label class="form-label">e) Meeting dead lines</label>
+                        <input type="text" name="rate_deadlines" class="form-control" placeholder="" required>
+                    </div>
+                    <div class="col-md-4">
+                        <label class="form-label">f) Relationship with other workers</label>
+                        <input type="text" name="rate_relationship_with_others" class="form-control" placeholder="" required>
+                    </div>
+                </div>
+                &nbsp;
+                <div class="row">
+                    <div class="col-md-12">
+                        <label class="form-label">Please, give your general/specific opinion on his/her employability</label>
+                        <textarea class="form-control" name="general_opinion" rows="2" placeholder="text here.."></textarea>
+
+                    </div>
+                </div>
+
+
+
+               &nbsp;
                 <div class="row">
                     <div class="col-md-4">
                         Thank you for your cooperation.
@@ -94,13 +94,13 @@
                     <div  class="col-md-4">
                         <div class="form-group">
                             <label class="form-label">Completed By:</label>
-                            <input type="text" name="ref_name" class="form-control" placeholder="Full Name" required>
+                            <input type="text" name="ref_name" class="form-control" placeholder="Full Name" required disabled>
                         </div>
                     </div>
                     <div  class="col-md-4">
                         <div class="form-group">
                             <label class="form-label">Email:</label>
-                            <input type="text" name="ref_email" class="form-control" placeholder="janedoe@example.com"  >
+                            <input type="text" name="ref_email" class="form-control" placeholder="janedoe@example.com" disabled >
                         </div>
                     </div>
                 </div>
