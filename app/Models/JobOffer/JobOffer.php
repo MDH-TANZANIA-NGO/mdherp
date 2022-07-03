bd6d2d28ccea9ca740382c6eef1f55c69d3aae26
Elinipendo Mziray <emziray@mdh.or.tz>
2022-07-03 15:49:17 +0300

    working on job offer

--- app/Models/JobOffer/JobOffer.php
+++ app/Models/JobOffer/JobOffer.php
@@ -3,9 +3,15 @@
 namespace App\Models\JobOffer;
 
 use App\Models\BaseModel;
+use App\Models\HumanResource\Interview\InterviewApplicant;
 use Illuminate\Database\Eloquent\Model;
 
 class JobOffer extends BaseModel
 {
     //
+
+    public function interviewApplicant()
+    {
+        return $this->belongsTo(InterviewApplicant::class ,'hr_hire_requisitions_job_applicants_id', 'applicant_id')
+    }
 }
