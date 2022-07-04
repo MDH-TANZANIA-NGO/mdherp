6e79df7787a5e6577f142a565adab93c9765b680
Elinipendo Mziray <emziray@mdh.or.tz>
2022-07-04 12:42:02 +0300

    working on job offer

--- resources/views/HumanResource/JobOffer/display/show.blade.php
+++ resources/views/HumanResource/JobOffer/display/show.blade.php
@@ -11,112 +11,59 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
-                    <h3 class="card-title">{{$job_offer->number}}</h3>
+                    <h3 class="card-title">Job Offer Details</h3>
                 </div>
                 <div class="card-body">
                     <div class="">
-                        <h4 class="mb-1">Hi <strong>Jessica Allen</strong>,</h4>
-                        This is the receipt for a payment of <strong>$450.00</strong> (USD) for your works
+                        <h4 class="mb-1"><strong>Jessica Allen</strong>,</h4>
+                        has been given offer of <strong>${{currency_converter($job_offer->salary, 'TSH')}}</strong> (USD) for job position as <b>ICT CUM Software Developer</b>
                     </div>
 
                     <div class="card-body pl-0 pr-0">
                         <div class="row">
                             <div class="col-sm-6">
-                                <span>Payment No.</span><br>
-                                <strong>INV23456-234</strong>
+                                <span>Job offer No.</span><br>
+                                <strong>{{$job_offer->number}}</strong>
                             </div>
                             <div class="col-sm-6 text-right">
-                                <span>Payment Date</span><br>
-                                <strong>Aug 10, 2019 - 12:20 pm</strong>
+                                <span>Expected arrival date</span><br>
+                                <strong>{{$job_offer->date_of_arrival}}</strong>
                             </div>
                         </div>
                     </div>
                     <div class="dropdown-divider"></div>
                     <div class="row pt-4">
                         <div class="col-lg-6 ">
-                            <p class="h3">Client Inc</p>
+                            <p class="h3">Other Benefits</p>
                             <address>
-                                Street Address<br>
-                                State, City<br>
-                                Region, Postal Code<br>
-                                ltd@example.com
+                                {!! htmlspecialchars_decode($job_offer->details) !!}
                             </address>
                         </div>
-                        <div class="col-lg-6 text-right">
-                            <p class="h3">Invoice To</p>
-                            <address>
-                                Street Address<br>
-                                State, City<br>
-                                Region, Postal Code<br>
-                                ctr@example.com
-                            </address>
-                        </div>
+
                     </div>
                     <div class="table-responsive push">
                         <table class="table table-bordered table-hover">
                             <tbody><tr class=" ">
-                                <th class="text-center " style="width: 1%"></th>
-                                <th>Product</th>
-                                <th class="text-center" style="width: 1%">Qnt</th>
-                                <th class="text-right" style="width: 1%">Unit</th>
-                                <th class="text-right" style="width: 1%">Amount</th>
+                                <th>Job Details</th>
                             </tr>
                             <tr>
-                                <td class="text-center">1</td>
                                 <td>
-                                    <p class="font-w600 mb-1">Logo Creation</p>
-                                    <div class="text-muted">Logo and business cards design</div>
+                                    <p class="font-w600 mb-1">Job Details to be set here</p>
                                 </td>
-                                <td class="text-center">2</td>
-                                <td class="text-right">$60.00</td>
-                                <td class="text-right">$120.00</td>
+
+                            </tr>
+
+                            <tr>
+                                <td colspan="1" class="font-weight-bold text-uppercase text-right">Working station : Tabora</td>
                             </tr>
                             <tr>
-                                <td class="text-center">2</td>
-                                <td>
-                                    <p class="font-w600 mb-1">Online Store Design &amp; Development</p>
-                                    <div class="text-muted">Design/Development for all popular modern browsers</div>
-                                </td>
-                                <td class="text-center">3</td>
-                                <td class="text-right">$80.00</td>
-                                <td class="text-right">$240.00</td>
-                            </tr>
-                            <tr>
-                                <td class="text-center">3</td>
-                                <td>
-                                    <p class="font-w600 mb-1">App Design</p>
-                                    <div class="text-muted">Promotional mobile application</div>
-                                </td>
-                                <td class="text-center">1</td>
-                                <td class="text-right">$40.00</td>
-                                <td class="text-right">$40.00</td>
-                            </tr>
-                            <tr>
-                                <td colspan="4" class="font-w600 text-right">Subtotal</td>
-                                <td class="text-right">$400.00</td>
-                            </tr>
-                            <tr>
-                                <td colspan="4" class="font-w600 text-right">Vat Rate</td>
-                                <td class="text-right">20%</td>
-                            </tr>
-                            <tr>
-                                <td colspan="4" class="font-w600 text-right">Vat Due</td>
-                                <td class="text-right">$50.00</td>
-                            </tr>
-                            <tr>
-                                <td colspan="4" class="font-weight-bold text-uppercase text-right">Total Due</td>
-                                <td class="font-weight-bold text-right">$450.00</td>
-                            </tr>
-                            <tr>
-                                <td colspan="5" class="text-right">
-                                    <button type="button" class="btn btn-primary" onclick="javascript:window.print();"><i class="si si-wallet"></i> Pay Invoice</button>
-                                    <button type="button" class="btn btn-secondary" onclick="javascript:window.print();"><i class="si si-paper-plane"></i> Send Invoice</button>
-                                    <button type="button" class="btn btn-info" onclick="javascript:window.print();"><i class="si si-printer"></i> Print Invoice</button>
+                                <td colspan="1" class="text-right">
+                                    <a href="{{route('job_offer.edit', $job_offer->uuid)}}" type="button" class="btn btn-secondary" ><i class="si si-paper-plane"></i> Edit</a>
+                                    <button type="button" class="btn btn-info" onclick="javascript:window.print();"><i class="si si-printer"></i> Print Offer</button>
                                 </td>
                             </tr>
                             </tbody></table>
                     </div>
-                    <p class="text-muted text-center">Thank you very much for doing business with us. We look forward to working with you again!</p>
                 </div>
             </div>
         </div>
