1f78d2aba80fd93aa521f727835730696a0f6797
Hamis Juma <hamisjuma1@gmail.com>
2022-07-04 15:07:51 +0300

    working on shortlisters

---
+++ app/Repositories/HumanResource/HireRequisition/HrUserHireRequisitionJobShortlisterRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Repositories\HumanResource\HireRequisition;
+
+use App\Models\HumanResource\HireRequisition\HrUserHireRequisitionJobShortlister;
+use App\Repositories\BaseRepository;
+
+class HrUserHireRequisitionJobShortlisterRepository extends BaseRepository
+{
+    const MODEL = HrUserHireRequisitionJobShortlister::class;
+}
