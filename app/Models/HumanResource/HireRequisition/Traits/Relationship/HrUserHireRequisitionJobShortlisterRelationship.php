ad5d909c9fc4c689267f1fa2c73491a93c4b448f
Hamis Juma <hamisjuma1@gmail.com>
2022-07-05 17:55:04 +0300

    Adding users to jobs

--- app/Models/HumanResource/HireRequisition/Traits/Relationship/HrUserHireRequisitionJobShortlisterRelationship.php
+++ app/Models/HumanResource/HireRequisition/Traits/Relationship/HrUserHireRequisitionJobShortlisterRelationship.php
@@ -3,6 +3,7 @@
 namespace App\Models\HumanResource\HireRequisition\Traits\Relationship;
 
 use App\Models\HumanResource\HireRequisition\HireRequisitionJob;
+use App\Models\HumanResource\HireRequisition\HrUserHireRequisitionJobShortlisterUser;
 
 trait HrUserHireRequisitionJobShortlisterRelationship
 {
@@ -10,4 +11,10 @@ trait HrUserHireRequisitionJobShortlisterRelationship
     {
         return $this->belongsTo(HireRequisitionJob::class,'hr_hire_requisitions_job_id','id');
     }
+
+    public function listUsers()
+    {
+        return $this->hasMany(HrUserHireRequisitionJobShortlisterUser::class,'hr_user_hire_requisition_job_shortlisters','id')
+    }
+
 }
