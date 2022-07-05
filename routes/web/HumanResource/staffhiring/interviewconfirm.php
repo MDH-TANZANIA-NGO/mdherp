7fde4925f1da4a5cd85869fb18277fa8c1563586
Frank Omary <91878811+frankomary@users.noreply.github.com>
2022-07-05 19:27:13 +0300

    working on updating comfirm

--- routes/web/HumanResource/staffhiring/interviewconfirm.php
+++ routes/web/HumanResource/staffhiring/interviewconfirm.php
@@ -2,4 +2,5 @@
 
 Route::group(['namespace' => 'humanResource\staffhiring', 'middleware' => ['web'], 'prefix' => 'interviewconfirm', 'as' => 'interviewconfirm.'], function () {
     Route::get('{applicant_id}/{interview_id}/interview', 'InterviewConfirmController@index')->name('index');
+    Route::get('{applicant_id},{interview_id}/interviewconf', 'InterviewConfirmController@update')->name('update');
 });
