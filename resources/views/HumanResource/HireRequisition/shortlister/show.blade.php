77b2ac56c8404745d00c7cba3e5021b2ab1c58eb
Hamis Juma <hamisjuma1@gmail.com>
2022-07-06 15:13:00 +0300

    complete Shortlisters workflow

--- resources/views/HumanResource/HireRequisition/shortlister/show.blade.php
+++ resources/views/HumanResource/HireRequisition/shortlister/show.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.app')
 @section('content')
 
-
+<div class="row mb-2">
+    <div class="col-lg-12">
+        @include('includes.workflow.workflow_track', ['current_wf_track' => $current_wf_track])
+    </div>
+</div>
 
 @endsection
