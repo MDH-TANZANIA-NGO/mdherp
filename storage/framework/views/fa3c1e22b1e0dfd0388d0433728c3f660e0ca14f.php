2ba57c602241fd6c8352415872996b59daea777d
Hamis Juma <hamisjuma1@gmail.com>
2022-07-06 12:55:59 +0300

    updating gitignore

--- storage/framework/views/fa3c1e22b1e0dfd0388d0433728c3f660e0ca14f.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('content'); ?>
-    <?php echo $__env->make('reports.hr.leave.datatable.index', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/elinipendomziray/Sites/mdherp/resources/views/reports/hr/leave/index.blade.php ENDPATH**/ ?>
