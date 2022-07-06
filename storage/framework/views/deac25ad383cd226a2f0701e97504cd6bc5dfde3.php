9802b64d4bd2635f0872e1776da727661c69d386
Frank Omary <91878811+frankomary@users.noreply.github.com>
2022-07-06 13:36:23 +0300

    Initiate new recruit

--- storage/framework/views/deac25ad383cd226a2f0701e97504cd6bc5dfde3.php
+++ storage/framework/views/deac25ad383cd226a2f0701e97504cd6bc5dfde3.php
@@ -3,7 +3,7 @@
       
     <div class="card">
         <div class="card-header">
-           APPLICANT LIST INVITED FOR INTERVIEW
+          APPLICANTS INVITED FOR INTERVIEW
         </div>
         <div class="card-body">
             
