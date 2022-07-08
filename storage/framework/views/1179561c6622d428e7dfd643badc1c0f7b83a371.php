d36d0833e07e61b063252b479e847de08a953a14
nkayogx <47517846+nkayogx@users.noreply.github.com>
2022-07-08 18:59:28 +0300

    none

--- storage/framework/views/1179561c6622d428e7dfd643badc1c0f7b83a371.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('content'); ?>
-    <?php echo $__env->make('induction._parent.datatables.index', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/william/Code/mdherp/resources/views/induction/_parent/index.blade.php ENDPATH**/ ?>
