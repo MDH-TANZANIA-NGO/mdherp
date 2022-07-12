2d7146efdd78426e0b59c25f1b933cb79d1d8790
Hamis Juma <hamisjuma1@gmail.com>
2022-07-12 12:38:15 +0300

    showing shortlisted and shortlisters

--- app/Models/HumanResource/HireRequisition/Traits/Relationship/HrHireRequisitionJobApplicantRelationship.php
+++ app/Models/HumanResource/HireRequisition/Traits/Relationship/HrHireRequisitionJobApplicantRelationship.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\HumanResource\HireRequisition\Traits\Relationship;
 
+use App\Models\Auth\User;
 use App\Models\HumanResource\HireRequisition\HrHireRequisitionJobApplicantRequest;
 
 trait HrHireRequisitionJobApplicantRelationship
@@ -9,5 +10,10 @@ trait HrHireRequisitionJobApplicantRelationship
     public function request()
     {
         return $this->belongsTo(HrHireRequisitionJobApplicantRequest::class,'hr_hire_requisition_job_applicant_request_id','id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
