0bbf4ad86857e6c303ca7f299c6b55c74858ec07
nkayogx <47517846+nkayogx@users.noreply.github.com>
2022-07-12 16:24:08 +0300

    migration added

---
+++ database/migrations/hr_requests/2022_07_12_161942_add_column_hr_requisition_job_id_skills_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnHrRequisitionJobIdSkillsUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('skill_user', function (Blueprint $table) {
+            $table->unsignedBigInteger('hr_requisition_job_id');
+            $table->smallInteger('skill_level_cv_id')->nullable()->change();
+            // $table->smallInteger('skill_level_cv_id');
+        });
+       
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
