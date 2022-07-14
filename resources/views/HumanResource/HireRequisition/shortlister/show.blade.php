9f9f5a9b090e2a746d6511b05e3129e886308306
Hamis Juma <hamisjuma1@gmail.com>
2022-07-14 13:38:31 +0300

    update shortlister on return

--- resources/views/HumanResource/HireRequisition/shortlister/show.blade.php
+++ resources/views/HumanResource/HireRequisition/shortlister/show.blade.php
@@ -17,6 +17,31 @@
 <div class="row mb-3">
     @include('HumanResource.HireRequisition.job._partials._job_description',['hire_requisition_job' => $job->job])
 
+    @if($can_edit_resource)
+    <div class="col-xl-12 col-md-12 col-md-12 col-lg-12">
+        {!! Form::open(['route' => ['job_shortlist_user.store',$job->id,$job->job->id] ]) !!}
+        <div class="card">
+            <div class="p-3">
+                <div class="col-xl-12 col-md-12 col-md-12 col-lg-12">
+                    <div class="form-group ">
+                        {!! Form::label('users[]', __("SELECT ONE OR MULTIPLE SHORTLISTER"),['class'=>'form-label','required_asterik']) !!}
+                        {!! Form::select('users[]', $users,
+                        App\Models\HumanResource\HireRequisition\HrUserHireRequisitionJobShortlisterUser::where('hr_user_hire_requisition_job_shortlister_id',$job->id)->pluck('user_id')
+                        , ['class' =>'form-control select2-show-search' , 'aria-describedby' => '','multiple', 'required']) !!}
+                        {!! $errors->first('users[]', '<span class="badge badge-danger">:message</span>') !!}
+                    </div>
+                </div>
+                <div class="col-xl-12 col-md-12 col-md-12 col-lg-12">
+                    <div class="form-group ">
+                        {!! Form::submit('Update',['class'=>'btn btn-primary']) !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+        {!! Form::close() !!}
+    </div>
+    @endif
+
     <div class="col-xl-12 col-md-12 col-md-12 col-lg-12">
         {!! Form::open(['route' => ['job_shortlist_user.store', $job->id,$job->job->id] ]) !!}
         <div class="card">
