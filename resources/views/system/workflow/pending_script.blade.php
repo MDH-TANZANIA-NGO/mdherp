027a2aab866fa26696badda8d58d3730b81cca0c
Hamis Juma <hamisjuma1@gmail.com>
2022-07-14 11:03:07 +0300

    update resource name request

--- resources/views/system/workflow/pending_script.blade.php
+++ resources/views/system/workflow/pending_script.blade.php
@@ -134,6 +134,9 @@
                         case 13: /*Shortlisters*/
                             window.open(base_url + "/human-resource/job-shortlisters/" + aData['resource_uuid']+ '/show');
                             break;
+                        case 13: /*Shortlisted Report*/
+                            window.open(base_url + "/human-resource/job-shortlisters/" + aData['resource_uuid']+ '/show');
+                            break;
                         default:
                             break;
                     }
