0fed866622448c7ef960138b3508ed0fefa7b458
Elinipendo Mziray <emziray@mdh.or.tz>
2022-07-26 15:54:04 +0300

    Create ActivityAttendanceRepository.php

---
+++ app/Repositories/Attendance/ActivityAttendanceRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories\Attendance;
+
+use App\Repositories\BaseRepository;
+
+class ActivityAttendanceRepository extends  BaseRepository
+{
+    public function __construct()
+    {
+        //
+    }
+}
