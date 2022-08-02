dc32de9392d10f2361e7f37a20edc5b5a75e34ca
Hamis Juma <hamisjuma1@gmail.com>
2022-08-02 09:59:43 +0300

    nothing

--- database/seeds/workflow/WfModulesTableSeeder.php
+++ database/seeds/workflow/WfModulesTableSeeder.php
@@ -286,6 +286,20 @@ class WfModulesTableSeeder extends Seeder
                     'allow_repeat' => 0,
                     'allow_decline' => 0,
                 ),
+            19 =>
+                array (
+                    'id' => 20,
+                    'name' => 'Performance Appraisal Goals/Objectives Agreement [ Managers for Regions ]',
+                    'wf_module_group_id' => '10',
+                    'created_at' => NULL,
+                    'updated_at' => NULL,
+                    'deleted_at' => NULL,
+                    'isactive' => '1',
+                    'type' => 1,
+                    'description' => 'Performance Review for Probatioinary Appraisal Goals/Objectives Agreement',
+                    'allow_repeat' => 0,
+                    'allow_decline' => 0,
+                ),
 
         ));
 
