034703297f5c17f106d3648bbbc82d2fae37ea15
Elinipendo Mziray <emziray@mdh.or.tz>
2022-08-09 09:12:43 +0300

    Update workspace.blade.php

--- resources/views/programactivity/workspace.blade.php
+++ resources/views/programactivity/workspace.blade.php
@@ -18,18 +18,18 @@
 
         </div>
 
-        <div class="col-4 col-sm-4 col-lg-3">
-            <a href="{{route('programactivityreport.index')}}">
-                {{--                <a href="{{route('safari.index')}}">--}}
-                <div class="card">
-                    <div class="card-body text-center">
-                        <div class="h2 m-0"><i class="zmdi zmdi-book multiple-outline text-primary" ></i></div>
-                        <div class="text-muted mb-0">Reports</div>
-                    </div>
-                </div>
-            </a>
+{{--        <div class="col-4 col-sm-4 col-lg-3">--}}
+{{--            <a href="{{route('programactivityreport.index')}}">--}}
+{{--                --}}{{--                <a href="{{route('safari.index')}}">--}}
+{{--                <div class="card">--}}
+{{--                    <div class="card-body text-center">--}}
+{{--                        <div class="h2 m-0"><i class="zmdi zmdi-book multiple-outline text-primary" ></i></div>--}}
+{{--                        <div class="text-muted mb-0">Reports</div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </a>--}}
 
-        </div>
+{{--        </div>--}}
         <div class="col-4 col-sm-4 col-lg-3">
             <a href="{{route('activity_report.index')}}">
                 <div class="card">
@@ -41,22 +41,22 @@
             </a>
 
         </div>
-        @if($supervisor != null)
-            @if(access()->user()->id == $supervisor->supervisor_id)
+{{--        @if($supervisor != null)--}}
+{{--            @if(access()->user()->id == $supervisor->supervisor_id)--}}
 
-                <div class="col-4 col-sm-4 col-lg-3">
-                    <a href="{{route('programactivity.reports')}}">
-                        <div class="card">
-                            <div class="card-body text-center">
-                                <div class="h2 m-0"><i class="zmdi zmdi-badge-check multiple-outline text-primary" ></i></div>
-                                <div class="text-muted mb-0">Report Approvals</div>
-                            </div>
-                        </div>
-                    </a>
+{{--                <div class="col-4 col-sm-4 col-lg-3">--}}
+{{--                    <a href="{{route('programactivity.reports')}}">--}}
+{{--                        <div class="card">--}}
+{{--                            <div class="card-body text-center">--}}
+{{--                                <div class="h2 m-0"><i class="zmdi zmdi-badge-check multiple-outline text-primary" ></i></div>--}}
+{{--                                <div class="text-muted mb-0">Report Approvals</div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </a>--}}
 
-                </div>
-            @endif
-        @endif
+{{--                </div>--}}
+{{--            @endif--}}
+{{--        @endif--}}
 
 
 
